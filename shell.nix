81abbb040e2af58c6f6a373aa5a3a74073f3bce5
Sumner Evans <me@sumnerevans.com>
2020-11-24 21:26:13 -0700

    Improve Nix shell

--- shell.nix
+++ shell.nix
@@ -1,12 +1,50 @@
-{ pkgs ? import <nixpkgs> {} }:
-pkgs.mkShell {
-  propagatedBuildInputs = with pkgs; [
-    (python38.withPackages (ps: with ps; [
+let
+  pkgs = import <nixpkgs> {};
+  py38WithPackages = pkgs.python38.withPackages (
+    ps: with ps; [
       pynvim
       flake8
       black
       graphviz
       python-language-server
-    ]))
+    ]
+  );
+
+  # CoC Config
+  cocConfig = {
+    "python.pythonPath" = "${py38WithPackages}/bin/python";
+    "python.jediPath" = "${py38WithPackages}/lib/python3.8/site-packages";
+  };
+in
+pkgs.mkShell {
+  shellHook = ''
+    # https://e.printstacktrace.blog/merging-json-files-recursively-in-the-command-line/
+    mkdir -p .vim
+    echo '${builtins.toJSON cocConfig}' |
+      ${pkgs.jq}/bin/jq -s \
+        'def deepmerge(a;b):
+          reduce b[] as $item (a;
+            reduce ($item | keys_unsorted[]) as $key (.;
+              $item[$key] as $val | ($val | type) as $type | .[$key] = if ($type == "object") then
+                deepmerge({}; [if .[$key] == null then {} else .[$key] end, $val])
+              elif ($type == "array") then
+                (.[$key] + $val | unique)
+              else
+                $val
+              end)
+            );
+          deepmerge({}; .)' \
+        .vim/coc-settings.part.json \
+        - \
+      > .vim/coc-settings.json
+  '';
+
+  buildInputs = with pkgs; [
+    # Core
+    rnix-lsp
+
+    py38WithPackages
+    py38WithPackages.pkgs.black
+    py38WithPackages.pkgs.flake8
   ];
 }
