e72b59e8f4d5f6412bdce1b9368f95973f869daa
Sumner Evans <me@sumnerevans.com>
2020-12-10 23:42:46 -0700

    Significantly cleaned up version of 2020.11

--- 2020/11.py
+++ 2020/11.py
@@ -21,41 +21,32 @@ INF = float("inf")
 
 
 # Input parsing
-A = [[x for x in l.strip()] for l in sys.stdin.readlines()]
-lines = copy.deepcopy(A)
+lines = [[x for x in l.strip()] for l in sys.stdin.readlines()]
+C = len(lines[0])
+R = len(lines)
 
 ########################################################################################
 print(f"\n{'=' * 30}\n")
 print("Part 1:")
 
+DIRS = [  # Tuples of (delta_row, delta_col)
+    (-1, 0),  # above
+    (1, 0),  # below
+    (0, -1),  # left
+    (0, 1),  # right
+    (-1, -1),  # top-left
+    (-1, 1),  # top-right
+    (1, -1),  # bottom-left
+    (1, 1),  # bottom-right
+]
+
 
 def adjs(r, c):
-    if r > 0:
-        yield r - 1, c
-        if c > 0:
-            yield r - 1, c - 1
-        if c < len(lines[0]) - 1:
-            yield r - 1, c + 1
-    if c > 0:
-        yield r, c - 1
-        if r > 0:
-            yield r - 1, c - 1
-        if r < len(lines) - 1:
-            yield r + 1, c - 1
-
-    if r < len(lines) - 1:
-        yield r + 1, c
-        if c > 0:
-            yield r + 1, c - 1
-        if c < len(lines[0]) - 1:
-            yield r + 1, c + 1
-
-    if c < len(lines[0]) - 1:
-        yield r, c + 1
-        if r < len(lines) - 1:
-            yield r + 1, c + 1
-        if r > 0:
-            yield r - 1, c + 1
+    # Iterate through all of the directions and return all of the (row, col) tuples
+    # representing the adjacent cells.
+    for dy, dx in DIRS:
+        if 0 <= r + dy < R and 0 <= c + dx < C:
+            yield r + dy, c + dx
 
 
 def part1():
@@ -66,27 +57,22 @@ def part1():
         for r in range(len(lines)):
             for c in range(len(lines[0])):
                 if prev[r][c] in "L#":
+                    # This cell is a seat that is either occupied or not.
+                    # Count the number of occupied adjacent seats.
                     num_o = 0
-                    for a_r, a_c in set(adjs(r, c)):
-                        assert a_r >= 0 and a_c >= 0
+                    for a_r, a_c in adjs(r, c):
                         if prev[a_r][a_c] == "#":
                             num_o += 1
 
                     if num_o == 0:
+                        # Nobody in adjacent seats, occupy.
                         new[r][c] = "#"
                     if num_o >= 4:
+                        # 4+ occupied adjacent seats. LEAVE!
                         new[r][c] = "L"
 
-        neq = False
-        for x, y in zip(prev, new):
-            for a, b in zip(x, y):
-                if a != b:
-                    neq = True
-                    break
-            if neq:
-                break
-
-        if not neq:
+        # Check to see if everything is equal. If it is, then we are at steady state.
+        if prev == new:
             # Return the count of all of the occupied seats (marked with '#')
             return "".join("".join(p) for p in prev).count("#")
 
@@ -111,87 +97,43 @@ print("\nPart 2:")
 
 
 def rays(r, c):
-    C = len(lines[0])
-    R = len(lines)
-
-    # right
-    if c < C:
-        yield [(r, x) for x in range(c + 1, C)]
-
-    # left
-    if c > 0:
-        yield [(r, x) for x in range(c - 1, -1, -1)]
-
-    # down
-    if r > 0:
-        yield [(x, c) for x in range(r - 1, -1, -1)]
-
-    # up
-    if r < R:
-        yield [(x, c) for x in range(r + 1, R)]
-
-    # diags
-    def diag(dx, dy):
+    def step(dy, dx):
+        # Step once to make sure you don't return the current cell.
         a = r + dy
         b = c + dx
-        x = []
         while a >= 0 and b >= 0 and a < R and b < C:
-            x.append((a, b))
+            yield a, b
             a += dy
             b += dx
-        return x
 
-    yield diag(1, 1)
-    yield diag(1, -1)
-    yield diag(-1, -1)
-    yield diag(-1, 1)
-    # print(diag(-1, 1))
-    # ohea
-
-
-# print("\n".join(map(str, rays(0, 9))))
-# ohea
+    # yield from == JavaScript yield*
+    # This yields all of the elements of the generator individually.
+    yield from (step(dy, dx) for (dy, dx) in DIRS)
 
 
 def part2():
     prev = copy.deepcopy(lines)
     i = 0
     while True:
-        # print()
-        # print('\n'.join(["".join(p) for p in prev]))
-        # if i > 2:
-        #     break
         new = copy.deepcopy(prev)
         for r in range(len(lines)):
             for c in range(len(lines[0])):
                 if prev[r][c] in "L#":
                     num_o = 0
-                    # if (r, c) == (1, 9):
-                    #     print(r, c, list(rays(r, c)))
                     for ray in rays(r, c):
-                        # print(ray)
                         for r_r, r_c in ray:
                             if prev[r_r][r_c] in "L#":
                                 if prev[r_r][r_c] == "#":
                                     num_o += 1
                                 break
 
-                    # print(num_o)
                     if num_o == 0:
                         new[r][c] = "#"
                     if num_o >= 5:
                         new[r][c] = "L"
 
-        neq = False
-        for x, y in zip(prev, new):
-            for a, b in zip(x, y):
-                if a != b:
-                    neq = True
-                    break
-            if neq:
-                break
-
-        if not neq:
+        # Check to see if everything is equal. If it is, then we are at steady state.
+        if prev == new:
             return "".join("".join(p) for p in prev).count("#")
 
         i += 1
@@ -199,7 +141,7 @@ def part2():
 
 
 ans_part2 = part2()
-print("ohea", ans_part2)
+print(ans_part2)
 
 # Store the attempts that failed here.
 tries2 = []
@@ -207,4 +149,4 @@ print("Tries Part 2:", tries2)
 assert ans_part2 not in tries2, "Same as an incorrect answer!"
 
 # Regression Test
-# assert test or ans_part2 == (<>)
+assert test or ans_part2 == 2208
