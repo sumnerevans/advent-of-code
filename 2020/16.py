a4c7f82b950b77c3f7fcbcf8c3613bbf07c3f35d
Sumner Evans <me@sumnerevans.com>
2020-12-16 00:24:08 -0700

    Add some explanations for 2020.16

--- 2020/16.py
+++ 2020/16.py
@@ -10,13 +10,17 @@ if len(sys.argv) > 1:
     if sys.argv[1] == "--test":
         test = True
 
-# Constants
+
 # Utilities
 def rematch(pattern: str, string: str) -> Optional[Match]:
     return re.fullmatch(pattern, string)
 
 
 # Input parsing
+# I used a state machine of sorts to parse the input today. There are definitely cleaner
+# ways of doing this, but this is what came to mind immediately.
+# Generally, if you have an input with a few different distinct parts, a state machine
+# like this is reasonable.
 lines: List[str] = [l.strip() for l in sys.stdin.readlines()]
 
 # Keep track of where we are in the file.
@@ -28,22 +32,22 @@ yours = []  # your ticket
 nearby = []  # the nearby tickets
 
 for line in lines:
-    if line.startswith("your") or line.startswith("nearby"):
+    if line.startswith("your") or line.startswith("nearby"):  # Discard these lines
         continue
-    if line == "":
+    if line == "":  # Update the state machine state if at an empty line
         if not endrules:
             endrules = True
         elif not endyours:
             endyours = True
         continue
 
-    if not endrules:
-        rn, *ranges = rematch(r"(.*): (\d+)-(\d+) or (\d+)-(\d+)", line).groups()
+    if not endrules:  # Parse the line as a rule <rule_name>: <bound1> or <bound2>
+        rule_name, *ranges = rematch(r"(.*): (\d+)-(\d+) or (\d+)-(\d+)", line).groups()
         ranges = [int(x) for x in ranges]
-        rules[rn] = (tuple(ranges[:2]), tuple(ranges[2:]))
-    elif not endyours:
+        rules[rule_name] = (tuple(ranges[:2]), tuple(ranges[2:]))
+    elif not endyours:  # This is our ticket, parse as a single list.
         yours = [int(x) for x in line.split(",")]
-    else:
+    else:  # This is somebody else's ticket, append to the nearby list.
         nearby.append([int(x) for x in line.split(",")])
 
 ########################################################################################
@@ -52,8 +56,11 @@ print("Part 1:")
 
 
 def part1() -> int:
+    # The scan error rate is just the sum of all the invalid fields.
     scan_error_rate = 0
     for ticket in nearby:
+        # Check that each of the fields have at least one rule for which they are in
+        # one of the ranges.
         for field in ticket:
             inrange = False
             for ranges in rules.values():
@@ -64,6 +71,8 @@ def part1() -> int:
                 if inrange:
                     break
 
+            # This field wasn't in any range, so add the field value to the
+            # scan_error_rate.
             if not inrange:
                 scan_error_rate += field
                 break
@@ -88,8 +97,11 @@ print("\nPart 2:")
 
 
 def part2() -> int:
+    # This variable stores a mapping of index to set of rules that that index could be.
     possibles = {i: set(rules.keys()) for i in range(len(nearby[0]))}
+
     for ticket in nearby:
+        # Detect if the ticket is invalid. If it is, then we won't process it.
         isinvalid = False
         for field in ticket:
             fieldvalid = False
@@ -109,6 +121,11 @@ def part2() -> int:
         if isinvalid:
             continue
 
+        # Go through all of the fields in the ticket and eliminate any possibilities
+        # that are impossible given the value of the field and ranges given by the
+        # rules.
+        # For example, if there is a rule "seat: 2-3 or 5-7", and index 0 of our ticket
+        # is 4, then we know that index 0 cannot be "seat".
         for i, field_value in enumerate(ticket):
             new_possibles = set(deepcopy(possibles[i]))
             for field_name in possibles[i]:
