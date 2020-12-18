4a14f8181a040b4b23d3d908f301a8b157c370f9
Sumner Evans <me@sumnerevans.com>
2020-12-17 23:11:54 -0700

    Add annotations for 18 and a bit of cleanup

--- 2020/18.py
+++ 2020/18.py
@@ -1,7 +1,7 @@
 #! /usr/bin/env python3
 
 import sys
-from typing import List, TypeVar
+from typing import List, Tuple, TypeVar
 
 test = False
 if len(sys.argv) > 1:
@@ -19,6 +19,7 @@ lines: List[str] = [l.strip() for l in sys.stdin.readlines()]
 
 def tokenize(l):
     tokens = []
+    # Iterating character-by-character because the numbers are only one digit.
     for c in l:
         if c in " ":
             continue
@@ -37,16 +38,59 @@ print(f"\n{'=' * 30}\n")
 print("Part 1:")
 
 
-def compute(tokens, i):
+"""
+An AST is an Abstract Syntax Tree. It is a tree representing the computation that
+needs to be performed. I did not intially use an AST, but this is an explanation of
+what it is.
+If you have the following computation: 1 + (2 * 3), then the tree would look something
+like this:
+                         (+)
+                        /   \
+                      (1)   (*)
+                            / \
+                          (2) (3)
+
+This is what I should have used, but instead, I basically implicitly navigated this tree
+using recursion, blood, sweat, and tears. In all honesty, though, this isn't a terrible
+approach because there is no real need to encode the order of operations in a tree.
+
+One nice thing about the first part that makes it a bit easier to do the implicit
+traversal is that there is no order of operations except for parentheses.
+"""
+
+
+def compute(tokens, i) -> Tuple[int, int]:
+    """
+    Parameters:
+    tokens: is the list of tokens
+    i: the index to start computing at
+
+    Returns:
+    A tuple of the computation result up to the next end parentheses, and the index at
+    which we finished computing.
+    """
+    # This is a really dumb way of implicitly navigating the AST. These variables
+    # basically keep track of the operation node.
     ismul = False
     isadd = False
 
-    x = None
+    # This accumulates the computation at a level of the computation.
+    x = 0
 
     while i < len(tokens):
         t = tokens[i]
-        if isinstance(t, int):
+        if isinstance(t, int) or t == "(":
+            # If this is an open-parentheses, recursively call the compute function to
+            # compute the value inside the parentheses.
+            if t == "(":
+                t, i = compute(tokens, i + 1)
+
+            # Perform the necessary operation on x
             if ismul:
+                # This is the case if we are in a situation like this:
+                #    4 * 3
+                #        ^
+                #        i
                 x *= t
                 ismul = False
             elif isadd:
@@ -54,16 +98,6 @@ def compute(tokens, i):
                 isadd = False
             else:
                 x = t
-        elif t == "(":
-            y, i = compute(tokens, i + 1)
-            if ismul:
-                x *= y
-                ismul = False
-            elif isadd:
-                x += y
-                isadd = False
-            else:
-                x = y
 
         elif t == ")":
             return x, i
@@ -104,6 +138,10 @@ print("\nPart 2:")
 
 def compute2(tokens, i):
     def addmul(terms):
+        """
+        Perform addition on all of the terms that need to be added and then do the
+        multiplication.
+        """
         while len(terms) > 1:
             if any(t == "+" for t in terms):
                 for i in range(len(terms)):
@@ -124,7 +162,7 @@ def compute2(tokens, i):
     terms = []
     while i < len(tokens):
         t = tokens[i]
-        if isinstance(t, int):
+        if isinstance(t, int) or t in "+*":
             terms.append(t)
         elif t == "(":
             t, i = compute2(tokens, i + 1)
@@ -132,8 +170,6 @@ def compute2(tokens, i):
 
         elif t == ")":
             return addmul(terms), i
-        elif t in "+*":
-            terms.append(t)
 
         i += 1
 
