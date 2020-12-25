cbe5465f82d67a9b5184d6d526705f0e0b5c21b2
Sumner Evans <me@sumnerevans.com>
2020-12-24 23:11:35 -0700

    Solve 2020.25

---
+++ 2020/25.py
@@ -0,0 +1,116 @@
+#! /usr/bin/env python3
+
+import functools as ft
+import itertools as it
+import heapq
+import math
+import operator
+import os
+import re
+import string
+import sys
+import time
+from copy import deepcopy
+from collections import defaultdict
+from enum import IntEnum
+from typing import (
+    Dict,
+    Generator,
+    Iterable,
+    List,
+    Match,
+    Optional,
+    Set,
+    Sized,
+    Tuple,
+    TypeVar,
+    Union,
+)
+
+test = False
+debug = False
+for arg in sys.argv:
+    if arg == "--test":
+        test = True
+    if arg == "--debug":
+        debug = True
+
+
+print(f"\n{'=' * 30}\n")
+
+# Input parsing
+input_start = time.time()
+
+lines: List[str] = [l.strip() for l in sys.stdin.readlines()]
+
+PK1 = int(lines[0])
+PK2 = int(lines[1])
+
+input_end = time.time()
+
+# Part 1
+########################################################################################
+print("Part 1:")
+
+
+def part1() -> int:
+    """
+    This problem involved doing efficient modular exponentiation. A memory efficient
+    method is required:
+    https://en.wikipedia.org/wiki/Modular_exponentiation#Memory-efficient_method
+
+    The base algorithm is as follows:
+    1. Set c = 1, e′ = 0.
+    2. Increase e′ by 1.
+    3. Set c = (b ⋅ c) mod m.
+    4. If e′ < e, go to step 2. Else, c contains the correct solution to c ≡ b^e (mod m)
+
+    There is one extra step for this, though, as during the above process, you have to
+    check c against the two public keys and once we have the e' corresponding to each of
+    the public keys, we can just exit the loop.
+    """
+    m = 20201227
+    c = 1
+    eprime = 0
+    ls1, ls2 = 0, 0
+    while True:
+        eprime += 1
+        c = (7 * c) % m
+        if c == PK1:
+            ls1 = eprime
+        elif c == PK2:
+            ls2 = eprime
+        if ls1 > 0 and ls2 > 0:
+            break
+
+    # Doing the base algorithm to find the answer here
+    c = 1
+    eprime = 0
+    while eprime < ls2:
+        eprime += 1
+        c = (PK1 * c) % 20201227
+    return c
+
+
+part1_start = time.time()
+ans_part1 = part1()
+part1_end = time.time()
+print(ans_part1)
+
+# Store the attempts that failed here.
+tries = []
+print("Tries Part 1:", tries)
+assert ans_part1 not in tries, "Same as an incorrect answer!"
+
+
+# Regression Test
+assert test or ans_part1 == 448851
+
+if debug:
+    input_parsing = input_end - input_start
+    part1_time = part1_end - part1_start
+    print()
+    print("DEBUG:")
+    print(f"Input parsing: {input_parsing * 1000}ms")
+    print(f"Part 1: {part1_time * 1000}ms")
+    print(f"TOTAL: {(input_parsing + part1_time) * 1000}ms")
