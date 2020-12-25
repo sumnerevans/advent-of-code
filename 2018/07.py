b2514e7c7d3ae4f99dcb8bab34ce1d84f8d0f7c2
Sumner Evans <me@sumnerevans.com>
2020-12-24 19:14:04 -0700

    Add annotations for 2018.07

--- 2018/07.py
+++ 2018/07.py
@@ -1,30 +1,13 @@
 #! /usr/bin/env python3
 
-import functools as ft
-import itertools as it
 import heapq
-import math
-import operator
 import re
 import string
 import sys
 import time
 from copy import deepcopy
 from collections import defaultdict
-from enum import IntEnum
-from typing import (
-    Dict,
-    Generator,
-    Iterable,
-    List,
-    Match,
-    Optional,
-    Set,
-    Sized,
-    Tuple,
-    TypeVar,
-    Union,
-)
+from typing import List, Match, Optional, Set, Tuple
 
 test = False
 debug = False
@@ -35,190 +18,8 @@ for arg in sys.argv:
         debug = True
 
 
-# Type variables
-K = TypeVar("K")
-V = TypeVar("V")
-
-
-# Utilities
-def cache():  # Python 3.9 compat
-    return ft.lru_cache(maxsize=None)
-
-
-def dijkstra(G: Dict[K, Iterable[Tuple[int, K]]], start: K, end: K) -> int:
-    """
-    A simple implementation of Dijkstra's shortest path algorithm for finding the
-    shortest path from ``start`` to ``end`` in ``G``.
-    """
-    Q = []
-    for k in G:
-        heapq.heappush(Q, (math.inf, k))
-    heapq.heappush(Q, (0, start))
-
-    D = {}
-    while Q:
-        cost, el = heapq.heappop(Q)
-        if cost < D.get(el, math.inf):
-            D[el] = cost
-
-        for c, x in G[el]:
-            heapq.heappush(Q, (cost + c, x))
-
-    return D[end]
-
-
-def prod(it: Iterable):
-    return ft.reduce(operator.mul, it, 1)
-
-
 def rematch(pattern: str, string: str) -> Optional[Match]:
     return re.fullmatch(pattern, string)
-
-
-def grid_adjs(
-    coord: Tuple[int, ...],
-    bounds: Tuple[Tuple[int, int], ...] = None,
-    inclusive: bool = True,
-) -> Generator[Tuple[int, ...], None, None]:
-    # Iterate through all of the deltas for the N dimensions of the coord. A delta is
-    # -1, 0, or 1 indicating that the adjacent cell is one lower, same level, or higher
-    # than the given coordinate.
-    for delta in it.product((-1, 0, 1), repeat=len(coord)):
-        if all(d == 0 for d in delta):
-            # This is the coord itself, skip.
-            continue
-
-        # Check the bounds
-        if bounds is not None:
-            inbounds = True
-            for i, (d, (low, high)) in enumerate(zip(delta, bounds)):
-                if inclusive and not (low <= coord[i] + d <= high):
-                    inbounds = False
-                    break
-                elif not inclusive and not (low < coord[i] + d < high):
-                    inbounds = False
-                    break
-            if not inbounds:
-                continue
-
-        yield tuple(c + d for c, d in zip(coord, delta))
-
-
-def manhattan(x1: int, y1: int, x2: int = 0, y2: int = 0) -> int:
-    return abs(x2 - x1) + abs(y2 - y1)
-
-
-def pbits(num: int, pad: int = 32) -> str:
-    """Return the bits of `num` in binary with the given padding."""
-    return bin(num)[2:].zfill(pad)
-
-
-def rot(
-    x: float, y: float, deg: float, origin: Tuple[float, float] = (0, 0)
-) -> Tuple[float, float]:
-    """
-    Rotate a point by `deg` around the `origin`. This does floating-point math, so
-    you may encounter precision errors.
-    """
-    theta = deg * math.pi / 180
-    x2 = (x - origin[0]) * math.cos(theta) - (y - origin[1]) * math.sin(theta)
-    y2 = (x - origin[0]) * math.sin(theta) + (y - origin[1]) * math.cos(theta)
-    return (x2 + origin[0], y2 + origin[1])
-
-
-def irot(x: int, y: int, deg: int, origin: Tuple[int, int] = (0, 0)) -> Tuple[int, int]:
-    """
-    Rotate an integer point by `deg` around the `origin`. Only works when deg % 90 == 0.
-    """
-    transformed_x = x - origin[0]
-    transformed_y = y - origin[1]
-    assert deg % 90 == 0
-    for _ in range((deg // 90) % 4):
-        transformed_x, transformed_y = -transformed_y, transformed_x
-    return (transformed_x + origin[0], transformed_y + origin[1])
-
-
-def sizezip(*iterables: Iterable) -> Generator[Tuple, None, None]:
-    assert len(set(len(x) for x in iterables)) == 1  # type: ignore
-    yield from zip(*iterables)
-
-
-def infer_one_to_one_from_possibles(possibles: Dict[K, Set[V]]):
-    """
-    This goes through a dictionary of key to potential values and computes the true
-    value using simple inference where if a key can only be a single value, then it must
-    be that value. For example:
-
-        A -> {X, Y}
-        B -> {Y}
-        C -> {X, Z}
-
-    then B -> Y, which means that A cannot be Y, thus A must be X, and by the same logic
-    C must be Z.
-    """
-    inferred = {}
-    while len(possibles):
-        # Find the alergen that only has one ingredient associated with it and pull it
-        # out of the possibles dictionary, and remove the ingredient from all of the
-        # other sets.
-        for idx, possible_fields in possibles.items():
-            if len(possible_fields) == 1:
-                inferred[idx] = possible_fields.pop()
-                remove_idx = idx
-                break
-        else:  # nobreak
-            assert False, "No keys have a single possible value"
-
-        del possibles[remove_idx]
-        for x in possibles:
-            if inferred[remove_idx] in possibles[x]:
-                possibles[x].remove(inferred[remove_idx])
-
-    return inferred
-
-
-# Harvard-Architecture Machine
-class OC(IntEnum):
-    """Opcodes for the Harvard-architecture machine."""
-
-    jmp = 0  # jump relative to PC+1
-    acc = 1  # update accumulator
-    nop = 2  # do nothing
-    trm = 3  # terminate program
-
-
-# Change if you add instructions
-assert len(OC) == 4
-Tape = List[Tuple[OC, Tuple[int, ...]]]
-
-
-def decode_tape(lines: List[str]) -> Tape:
-    return [(OC[c], tuple(int(v) for v in vals)) for c, *vals in map(str.split, lines)]
-
-
-def run_harvard(tape: Tape, return_acc_if_loop: bool = True):
-    a = 0
-    pc = 0
-    seen = set()
-    while True:
-        if pc in seen:
-            return a if return_acc_if_loop else None
-
-        seen.add(pc)
-
-        oc, vs = tape[pc]
-        if oc == OC.trm:
-            return a
-        elif oc == OC.jmp:
-            pc += vs[0] - 1
-        elif oc == OC.acc:
-            a += vs[0]
-        elif oc == OC.nop:
-            pass
-
-        pc += 1
-
-    return a
 
 
 # Input parsing
@@ -229,13 +30,13 @@ STEP_DEPENDENCIES = defaultdict(set)
 STEP_SUBSEQUENTS = defaultdict(set)
 STEPS = set()
 for line in lines:
-    x, y = rematch(
+    step, dep = rematch(
         "Step (.) must be finished before step (.) can begin.", line
     ).groups()
-    STEPS.add(x)
-    STEPS.add(y)
-    STEP_DEPENDENCIES[y].add(x)
-    STEP_SUBSEQUENTS[x].add(y)
+    STEPS.add(step)
+    STEPS.add(dep)
+    STEP_DEPENDENCIES[dep].add(step)
+    STEP_SUBSEQUENTS[step].add(dep)
 STARTS = sorted(STEPS - set(STEP_DEPENDENCIES.keys()))
 
 input_end = time.time()
@@ -264,14 +65,27 @@ print("Part 1:")
 
 
 def part1() -> str:
+    """
+    I'm solving Part 1 using a BFS-like algorithm. I maintain a *frontier*, effectively
+    the set of elements that can be performed next. The rules state if there are
+    multiple jobs that can be perfomred at once, the one that is lexicographicaly first
+    should be performed. To accomplish this, I'm storing the frontier in a heap so that
+    the lexicographicaly first element is always what gets popped of the heap.
+
+    I'm pretty sure some sort of topological sort would also do the trick here.
+    """
     frontier = deepcopy(STARTS)
     heapq.heapify(frontier)
     satisfied = set()
     s = ""
     while frontier:
         current = heapq.heappop(frontier)
+
+        # If it's already been satisfied, skip.
         if current in satisfied:
             continue
+
+        # Check to see if all of the current job's dependencies have been satisfied.
         sat = True
         for dep in STEP_DEPENDENCIES[current]:
             if dep not in satisfied:
@@ -279,8 +93,13 @@ def part1() -> str:
                 break
         if not sat:
             continue
+
+        # Set the current job to satisfied, and add it to the ordering of the steps.
         satisfied.add(current)
         s += current
+
+        # Add all of the jobs that depend on this job to the frontier. (It is fine if
+        # they are not fully satisfied because of the sat check above.)
         for sub in STEP_SUBSEQUENTS[current]:
             heapq.heappush(frontier, sub)
 
@@ -311,38 +130,70 @@ print("\nPart 2:")
 
 
 def part2() -> int:
+    """
+    I'm doing Part 2 using a discrete event simulation (DES). I'm assuming there's a way
+    to do this using a topological sort as well, but I wanted to implement a DES.
+
+    The basic idea of a DES is that you keep a priority queue of "events". Each event is
+    a *discrete event* and mutates the state of the world in some way. In my case, the
+    state of the world consists of how many workers are available; the set of jobs that
+    are unstarted, started, and satisfied; and the time of the last completed job.
+    """
+    # Calculate the time it will take for each job to complete. For the full problem,
+    # the time is 60 + (numeric letter of the alphabet, starting at 1)
     BASE_TIME = 60 if not test else 0
     WORKERS = 6 if not test else 2
     job_to_time = {c: BASE_TIME + ord(c) - ord("A") + 1 for c in string.ascii_uppercase}
 
+    # World state
     T = 0
-    remaining = set(ans_part1)
-    satisfied = set()
-    Q = []
-    for i in range(WORKERS):
-        heapq.heappush(Q, (0, "WORKER AVAILABLE", i))
+    unstarted: Set[str] = set(ans_part1)
+    satisfied: Set[str] = set()
+    workers_available: int = 0
+    started: Set[str] = set()
 
-    workers_available = 0
-    doing = set()
+    # Create the event queue. The queue contains tuples of (time, action, ID) where time
+    # is the time at which the event occurs (note that this is the primary sort key in
+    # the priority queue), action is the action type, and ID is an optional arbitrary ID
+    # (used for the JOB DONE event).
+
+    # We start by populating the queue with an inital set of events: one event for each
+    # worker becoming available at time = 0.
+    Q: List[Tuple[int, str, str]] = [
+        (0, "WORKER AVAILABLE", "") for _ in range(WORKERS)
+    ]
+
+    # Loop through all of the events.
     while Q:
-        t, action, id_ = heapq.heappop(Q)
+        t, action, id_ = heapq.heappop(Q) # extract the next event
+
+        # Handle the different actions
         if action == "JOB DONE":
-            doing.remove(id_)
-            satisfied.add(id_)
-            T = max(T, t)
+            started.remove(id_)  # remove from the set of jobs currently being performed
+            satisfied.add(id_)  # add to the set of satisfied/completed jobs
+            T = max(T, t)  # update the max time
         elif action == "WORKER AVAILABLE":
+            # A worker has become available, increase the number of available workers.
             workers_available += 1
         elif action == "WORKER START":
-            if id_ in doing:
+            if id_ in started:
+                # Skip if already being worked on.
                 continue
-            workers_available -= 1
-            doing.add(id_)
-            remaining.remove(id_)
-            heapq.heappush(Q, (t + job_to_time[id_], "JOB DONE", id_))
-            heapq.heappush(Q, (t + job_to_time[id_], "WORKER AVAILABLE", 0))
 
-        if workers_available > 0 and remaining:
-            for r in remaining:
+            # Start work on the job.
+            started.add(id_)
+            unstarted.remove(id_)
+            workers_available -= 1  # the worker is no longer available
+
+            # Look up how long it will be until the job is completed. At that time, the
+            # job will be done and the worker will become available. Add these events to
+            # the queue.
+            heapq.heappush(Q, (t + job_to_time[id_], "JOB DONE", id_))
+            heapq.heappush(Q, (t + job_to_time[id_], "WORKER AVAILABLE", ""))
+
+        # Then, check if we can have a worker start on a job.
+        if workers_available > 0 and unstarted:
+            for r in unstarted:
                 # Can we actually do this job?
                 issat = True
                 for dep in STEP_DEPENDENCIES[r]:
