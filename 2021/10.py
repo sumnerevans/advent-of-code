2cc4e5f2f2140a6101544c64c5ba27c9b1ff8de0
Sumner Evans <me@sumnerevans.com>
2021-12-09 22:41:41 -0700

    2021/10: solve part 1 and 2

---
+++ 2021/10.py
@@ -0,0 +1,196 @@
+#! /usr/bin/env python3
+
+import functools as ft
+import sys
+import time
+from typing import List, Optional
+
+test = True
+debug = False
+stdin = False
+INFILENAME = "inputs/10.txt"
+TESTFILENAME = "inputs/10.test.txt"
+for arg in sys.argv:
+    if arg == "--notest":
+        test = False
+    if arg == "--debug":
+        debug = True
+    if arg == "--stdin":
+        stdin = True
+
+
+class bcolors:
+    HEADER = "\033[95m"
+    OKBLUE = "\033[94m"
+    OKCYAN = "\033[96m"
+    OKGREEN = "\033[92m"
+    WARNING = "\033[93m"
+    FAIL = "\033[91m"
+    ENDC = "\033[0m"
+    BOLD = "\033[1m"
+    UNDERLINE = "\033[4m"
+
+
+print(f"\n{'=' * 30}\n")
+
+# Read the input
+if stdin:
+    input_lines: List[str] = [l.strip() for l in sys.stdin.readlines()]
+else:
+    with open(INFILENAME) as f:
+        input_lines: List[str] = [l.strip() for l in f.readlines()]
+
+# Try and read in the test file.
+try:
+    with open(TESTFILENAME) as f:
+        test_lines: List[str] = [l.strip() for l in f.readlines()]
+except Exception:
+    test_lines = []
+
+
+# Shared
+########################################################################################
+
+
+# Part 1
+########################################################################################
+print("Part 1:")
+
+
+def part1(lines: List[str]) -> int:
+    ans = 0
+
+    for line in lines:
+        stack = []
+        closing_char = {"(": ")", "[": "]", "{": "}", "<": ">"}
+        penalties = {")": 3, "]": 57, "}": 1197, ">": 25137}
+        for c in line:
+            if c in "([{<":
+                stack.append(c)
+            else:
+                if closing_char[stack[-1]] != c:
+                    ans += penalties[c]
+                    break
+                else:
+                    stack.pop()
+
+    return ans
+
+
+# Run test on part 1
+if test:
+    print("Running test... ", end="")
+    if not test_lines:
+        print(f"{bcolors.FAIL}No test configured!{bcolors.ENDC}")
+    else:
+        test_ans_part1 = part1(test_lines)
+        print(test_ans_part1)
+        expected = 26397
+        if expected is None:
+            print(f"{bcolors.FAIL}No test configured!{bcolors.ENDC}")
+        elif test_ans_part1 == expected:
+            print(f"{bcolors.OKGREEN}PASS{bcolors.ENDC}")
+        else:
+            print(f"{bcolors.FAIL}FAIL{bcolors.ENDC}")
+            print(f"{bcolors.FAIL}Result: {test_ans_part1}{bcolors.ENDC}")
+            assert False
+
+        print("Result:", test_ans_part1)
+        print()
+
+part1_start = time.time()
+print("Running input...")
+ans_part1 = part1(input_lines)
+part1_end = time.time()
+print("Result:", ans_part1)
+
+tries = [
+    # Store the attempts that failed here.
+]
+if tries:
+    print("Tries Part 1:", tries)
+    assert ans_part1 not in tries, "Same as an incorrect answer!"
+
+
+# Regression Test
+expected = 464991
+if expected is not None:
+    assert ans_part1 == expected
+
+# Part 2
+########################################################################################
+print("\nPart 2:")
+
+
+def part2(lines: List[str]) -> int:
+    def score_line(line) -> Optional[int]:
+        stack = []
+        closing_char = {"(": ")", "[": "]", "{": "}", "<": ">"}
+        score = {"(": 1, "[": 2, "{": 3, "<": 4}
+        for c in line:
+            if c in "([{<":
+                stack.append(c)
+            else:
+                if closing_char[stack[-1]] == c:
+                    stack.pop()
+                else:
+                    return None
+
+        return ft.reduce(lambda t, c: t * 5 + score[c], reversed(stack), 0)
+
+    scores = []
+    for line in lines:
+        score = score_line(line)
+        if score is not None:
+            scores.append(score)
+    scores.sort()
+
+    return scores[len(scores) // 2]
+
+
+# Run test on part 2
+if test:
+    print("Running test... ", end="")
+    if not test_lines:
+        print(f"{bcolors.FAIL}No test configured!{bcolors.ENDC}")
+    else:
+        test_ans_part2 = part2(test_lines)
+        expected = 288957
+        if expected is None:
+            print(f"{bcolors.FAIL}No test configured!{bcolors.ENDC}")
+        elif test_ans_part2 == expected:
+            print(f"{bcolors.OKGREEN}PASS{bcolors.ENDC}")
+        else:
+            print(f"{bcolors.FAIL}FAIL{bcolors.ENDC}")
+            print(f"{bcolors.FAIL}Result: {test_ans_part2}{bcolors.ENDC}")
+            assert False
+
+        print("Result:", test_ans_part2)
+        print()
+
+part2_start = time.time()
+print("Running input...")
+ans_part2 = part2(input_lines)
+part2_end = time.time()
+print("Result:", ans_part2)
+
+tries2 = [
+    # Store the attempts that failed here.
+]
+if tries2:
+    print("Tries Part 2:", tries2)
+    assert ans_part2 not in tries2, "Same as an incorrect answer!"
+
+# Regression Test
+expected = 3662008566
+if expected is not None:
+    assert ans_part2 == expected
+
+if debug:
+    part1_time = part1_end - part1_start
+    part2_time = part2_end - part2_start
+    print()
+    print("DEBUG:")
+    print(f"Part 1: {part1_time * 1000}ms")
+    print(f"Part 2: {part2_time * 1000}ms")
+    print(f"TOTAL: {(part1_time + part2_time) * 1000}ms")
