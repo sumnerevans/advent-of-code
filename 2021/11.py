2592bfcd5be34b7f6448b8114c3b461dc32a94d4
Sumner Evans <me@sumnerevans.com>
2021-12-10 23:37:16 -0700

    2021/11: more cleanup

--- 2021/11.py
+++ 2021/11.py
@@ -1,32 +1,10 @@
 #! /usr/bin/env python3
 
-import functools as ft
 import itertools as it
-import heapq
-import math
-import operator
-import os
-import re
-import string
 import sys
 import time
-from copy import deepcopy
-from collections import defaultdict
-from enum import Enum, IntEnum
-from fractions import Fraction
-from typing import (
-    Callable,
-    Dict,
-    Generator,
-    Iterable,
-    List,
-    Match,
-    Optional,
-    Set,
-    Tuple,
-    TypeVar,
-    Union,
-)
+from enum import Enum
+from typing import Iterable, List, Tuple
 
 test = True
 debug = False
@@ -52,11 +30,6 @@ class bcolors:
     ENDC = "\033[0m"
     BOLD = "\033[1m"
     UNDERLINE = "\033[4m"
-
-
-# Type variables
-K = TypeVar("K")
-V = TypeVar("V")
 
 
 # Enums
@@ -86,88 +59,7 @@ class AdjacenciesType(Enum):
     ALL = "all"
 
 
-# Modified range functions
-def irange(start, end=None, step=1) -> Generator[int, None, None]:
-    """Inclusive range function."""
-    if end is None:
-        start, end = 0, start
-    yield from range(start, end + 1, step=step)
-
-
-def dirange(start, end=None, step=1) -> Generator[int, None, None]:
-    """
-    Directional, inclusive range. This range function is an inclusive version of
-    :class:`range` that figures out the correct step direction to make sure that it goes
-    from `start` to `end`, even if `end` is before `start`.
-
-    >>> dirange(2, -2)
-    [2, 1, 0, -1, -2]
-    >>> dirange(-2)
-    [0, -1, -2]
-    >>> dirange(2)
-    [0, 1, 2]
-    """
-    assert step > 0
-    if end is None:
-        start, end = 0, start
-
-    if end >= start:
-        yield from irange(start, end, step)
-    else:
-        yield from range(start, end - 1, step=-step)
-
-
 # Utilities
-def bitstrtoint(s: Union[str, List[Union[int, str, bool]]]) -> int:
-    if isinstance(s, list):
-        if isinstance(s[0], bool):
-            s = list(map(int, s))
-
-        s = "".join(map(str, s))
-    return int(s, 2)
-
-
-def cache():  # Python 3.9 compat
-    return ft.lru_cache(maxsize=None)
-
-
-def chunks(iterable, n):
-    if n < 1:
-        raise Exception("not allowed")
-    itertype = type(iterable) if type(iterable) in (list, set, tuple) else list
-
-    container = []
-    for x in iterable:
-        container.append(x)
-        if len(container) == n:
-            yield itertype(container)
-            container = []
-
-    if len(container) > 0:
-        yield itertype(container)
-
-
-def dijkstra(G: Dict[K, Iterable[Tuple[int, K]]], start: K, end: K) -> int:
-    """
-    A simple implementation of Dijkstra's shortest path algorithm for finding the
-    shortest path from ``start`` to ``end`` in ``G``.
-    """
-    Q = []
-    for k in G:
-        heapq.heappush(Q, (math.inf, k))
-    heapq.heappush(Q, (0, start))
-
-    D = {}
-    while Q:
-        cost, el = heapq.heappop(Q)
-        if cost < D.get(el, math.inf):
-            D[el] = cost
-            for c, x in G[el]:
-                heapq.heappush(Q, (cost + c, x))
-
-    return D[end]
-
-
 def grid_adjs(
     coord: Tuple[int, ...],
     bounds: Tuple[Tuple[int, int], ...] = None,
@@ -192,6 +84,12 @@ def grid_adjs(
             # This is the coord itself, skip.
             continue
 
+        if adjs_type == AdjacenciesType.COMPASS:
+            if sum(map(abs, delta)) > 1:
+                # For compass adjacencies, we only care when there's only one dimension
+                # different than the coordinate.
+                continue
+
         if bounds is not None:
             in_bounds = True
             for i, (d, (low, high)) in enumerate(zip(delta, bounds)):
@@ -207,156 +105,6 @@ def grid_adjs(
             if not in_bounds:
                 continue
         yield tuple(c + d for c, d in zip(coord, delta))
-
-
-def infer_one_to_one_from_possibles(possibles: Dict[K, Set[V]]) -> Dict[K, V]:
-    """
-    This goes through a dictionary of key to potential values and computes the true
-    value using simple inference where if a key can only be a single value, then it must
-    be that value. For example::
-
-        A -> {X, Y}
-        B -> {Y}
-        C -> {X, Z}
-
-    then ``B`` must be ``Y``, which means that ``A`` cannot be ``Y``, thus ``A`` must be
-    ``X``, and by the same logic ``C`` must be ``Z``.
-    """
-    inferred = {}
-    while len(possibles):
-        # Find the item that only has one possibility associated with it and pull it out
-        # of the possibles dictionary, and remove the ingredient from all of the other
-        # sets.
-        for key, possible_fields in possibles.items():
-            if len(possible_fields) == 1:
-                inferred[key] = possible_fields.pop()
-                remove_item = inferred[key]
-                del possibles[key]
-                break
-        else:  # nobreak
-            assert False, "No keys have a single possible value"
-
-        for x in possibles:
-            if remove_item in possibles[x]:
-                possibles[x].remove(remove_item)
-
-    return inferred
-
-
-def int_points_between(
-    start: Tuple[int, int], end: Tuple[int, int]
-) -> Generator[Tuple[int, int], None, None]:
-    """
-    Return a generator of all of the integer points between two given points. Note that
-    you are *not* guaranteed that the points will be given from `start` to `end`, but
-    all points will be included.
-    """
-    x1, y1 = start
-    x2, y2 = end
-    if x1 == x2:
-        yield from ((x1, y) for y in dirange(y1, y2))
-    elif y1 == y2:
-        yield from ((x, y1) for x in dirange(x1, x2))
-    else:
-        # If `x1 > x2`, that means that `start` is to the right of `end`, so we need to
-        # switch the points around so iteration always goes in the positive `x`
-        # direction.
-        if x1 > x2:
-            x1, x2, y1, y2 = x2, x1, y2, y1
-        dy = y2 - y1
-        dx = x2 - x1
-        slope = Fraction(dy, dx)
-        for i in irange(dy // slope.numerator):
-            yield (x1 + (i * slope.denominator), y1 + (i * slope.numerator))
-
-
-def invert_dict(d: Dict[K, V]) -> Dict[V, K]:
-    return {v: k for k, v in d.items()}
-
-
-def invert_graph(graph: Dict[K, Iterable[V]]) -> Dict[V, Set[K]]:
-    new_graph = {}
-    for k, vals in graph.items():
-        for v in vals:
-            if v not in new_graph:
-                new_graph[v] = set()
-            new_graph[v].add(k)
-    return new_graph
-
-
-def irot(x: int, y: int, deg: int, origin: Tuple[int, int] = (0, 0)) -> Tuple[int, int]:
-    """
-    Rotate an integer point ``(x, y)`` by ``deg`` around the ``origin``. Only works when
-    ``deg % 90 == 0``.
-    """
-    transformed_x = x - origin[0]
-    transformed_y = y - origin[1]
-    assert deg % 90 == 0
-    for _ in range((deg // 90) % 4):
-        transformed_x, transformed_y = -transformed_y, transformed_x
-    return (transformed_x + origin[0], transformed_y + origin[1])
-
-
-def manhattan(x1: int, y1: int, x2: int = 0, y2: int = 0) -> int:
-    return abs(x2 - x1) + abs(y2 - y1)
-
-
-def maplist(fn: Callable[[K], V], l: Iterable[K]) -> List[V]:
-    return list(map(fn, l))
-
-
-def pbits(num: int, pad: int = 32) -> str:
-    """Return the bits of `num` in binary with the given padding."""
-    return bin(num)[2:].zfill(pad)
-
-
-def prod(it: Iterable):
-    return ft.reduce(operator.mul, it, 1)
-
-
-def rematch(pattern: str, s: str) -> Optional[Match]:
-    return re.fullmatch(pattern, s)
-
-
-def rot(
-    x: float, y: float, deg: float, origin: Tuple[float, float] = (0, 0)
-) -> Tuple[float, float]:
-    """
-    Rotate a point by `deg` around the `origin`. This does floating-point math, so
-    you may encounter precision errors.
-    """
-    theta = deg * math.pi / 180
-    x2 = (x - origin[0]) * math.cos(theta) - (y - origin[1]) * math.sin(theta)
-    y2 = (x - origin[0]) * math.sin(theta) + (y - origin[1]) * math.cos(theta)
-    return (x2 + origin[0], y2 + origin[1])
-
-
-def seqminmax(sequence: Iterable[int]) -> Tuple[int, int]:
-    """
-    Returns a tuple containing the minimum and maximum element of the ``sequence``.
-    """
-    min_, max_ = math.inf, -math.inf
-    for x in sequence:
-        min_ = min(min_, x)
-        max_ = max(max_, x)
-    return int(min_), int(max_)
-
-
-def sizezip(*iterables: Union[List, Set]) -> Iterable[Tuple]:
-    """
-    Same as the :class:`zip` function, but verifies that the lengths of the
-    :class:`list`s or :class:`set`s are the same.
-    """
-    assert len(set(len(x) for x in iterables)) == 1
-    yield from zip(*iterables)
-
-
-def window(iterable: List[K], n: int) -> Iterable[Tuple[K, ...]]:
-    """
-    Return a sliding window of size ``n`` of the given iterable.
-    """
-    for start_idx in range(len(iterable) - n + 1):
-        yield tuple(iterable[start_idx + idx] for idx in range(n))
 
 
 print(f"\n{'=' * 30}\n")
