29029714b35ac029e2fa91f4ac8e74dd6848c27e
Sumner Evans <me@sumnerevans.com>
2022-10-02 00:41:41 -0600

    even more improvements
    Signed-off-by: Sumner Evans <me@sumnerevans.com>

--- y2021/d01/01_test.go
+++ y2021/d01/01_test.go
@@ -6,7 +6,6 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
-	"github.com/stretchr/testify/require"
 
 	"github.com/sumnerevans/advent-of-code/util"
 	"github.com/sumnerevans/advent-of-code/y2021/d01"
@@ -15,54 +14,68 @@ import (
 //go:embed *.txt
 var inputs embed.FS
 
-func Test_Part1(t *testing.T) {
+func Test_Day01(t *testing.T) {
 	log, sample, actual := util.SetupTest(t, inputs, "01")
 
-	if len(sample) > 0 {
-		ok := t.Run("Test case", func(t *testing.T) {
+	ok := t.Run("Part 1", func(t *testing.T) {
+		if len(sample) > 0 {
+			ok := t.Run("1 Test case", func(t *testing.T) {
+				day01 := &d01.Day01{}
+				err := day01.LoadInput(log, sample)
+				assert.NoError(t, err)
+				output := day01.Part1(log)
+
+				assert.EqualValues(t, 7, output)
+			})
+			if !ok {
+				t.FailNow()
+				return
+			}
+		}
+
+		t.Run("2 Actual input", func(t *testing.T) {
 			day01 := &d01.Day01{}
-			err := day01.LoadInput(log, sample)
+			err := day01.LoadInput(log, actual)
 			assert.NoError(t, err)
 			output := day01.Part1(log)
+			fmt.Print("=================================\n\n")
+			fmt.Printf("Part 1:\n%d", output)
+			fmt.Print("\n\n=================================\n")
 
-			assert.EqualValues(t, 7, output)
+			assert.EqualValues(t, 1502, output)
 		})
-		require.True(t, ok)
+	})
+	if !ok {
+		t.FailNow()
+		return
 	}
 
-	t.Run("Actual input", func(t *testing.T) {
-		day01 := &d01.Day01{}
-		err := day01.LoadInput(log, actual)
-		assert.NoError(t, err)
-		output := day01.Part1(log)
-		fmt.Printf("\nPart 1:\n%d\n", output)
+	t.Run("Part 2", func(t *testing.T) {
+		if len(sample) > 0 {
+			ok := t.Run("1 Test case", func(t *testing.T) {
+				day01 := &d01.Day01{}
+				err := day01.LoadInput(log, sample)
+				assert.NoError(t, err)
+				output := day01.Part2(log)
 
-		assert.EqualValues(t, 1502, output)
-	})
-}
+				assert.EqualValues(t, 5, output)
+			})
+			if !ok {
+				t.FailNow()
+				return
+			}
+		}
 
-func Test_Part2(t *testing.T) {
-	log, sample, actual := util.SetupTest(t, inputs, "01")
-
-	if len(sample) > 0 {
-		ok := t.Run("Test case", func(t *testing.T) {
+		t.Run("2 Actual input", func(t *testing.T) {
 			day01 := &d01.Day01{}
-			err := day01.LoadInput(log, sample)
+			err := day01.LoadInput(log, actual)
 			assert.NoError(t, err)
 			output := day01.Part2(log)
+			fmt.Print("=================================\n\n")
+			fmt.Printf("Part 2:\n%d", output)
+			fmt.Print("\n\n=================================\n")
 
-			assert.EqualValues(t, 5, output)
+			assert.EqualValues(t, 1538, output)
 		})
-		require.True(t, ok)
-	}
-
-	t.Run("Actual input", func(t *testing.T) {
-		day01 := &d01.Day01{}
-		err := day01.LoadInput(log, actual)
-		assert.NoError(t, err)
-		output := day01.Part2(log)
-		fmt.Printf("\nPart 2:\n%d\n", output)
-
-		assert.EqualValues(t, 1538, output)
 	})
 }
--- y2021/d02/02_test.go
+++ y2021/d02/02_test.go
@@ -6,7 +6,6 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
-	"github.com/stretchr/testify/require"
 
 	"github.com/sumnerevans/advent-of-code/util"
 	"github.com/sumnerevans/advent-of-code/y2021/d02"
@@ -15,54 +14,68 @@ import (
 //go:embed *.txt
 var inputs embed.FS
 
-func Test_Part1(t *testing.T) {
+func Test_Day02(t *testing.T) {
 	log, sample, actual := util.SetupTest(t, inputs, "02")
 
-	if len(sample) > 0 {
-		ok := t.Run("Test case", func(t *testing.T) {
+	ok := t.Run("Part 1", func(t *testing.T) {
+		if len(sample) > 0 {
+			ok := t.Run("1 Test case", func(t *testing.T) {
+				day02 := &d02.Day02{}
+				err := day02.LoadInput(log, sample)
+				assert.NoError(t, err)
+				output := day02.Part1(log)
+
+				assert.EqualValues(t, 150, output)
+			})
+			if !ok {
+				t.FailNow()
+				return
+			}
+		}
+
+		t.Run("2 Actual input", func(t *testing.T) {
 			day02 := &d02.Day02{}
-			err := day02.LoadInput(log, sample)
+			err := day02.LoadInput(log, actual)
 			assert.NoError(t, err)
 			output := day02.Part1(log)
+			fmt.Print("=================================\n\n")
+			fmt.Printf("Part 1:\n%d", output)
+			fmt.Print("\n\n=================================\n")
 
-			assert.EqualValues(t, 150, output)
+			assert.EqualValues(t, 2073315, output)
 		})
-		require.True(t, ok)
+	})
+	if !ok {
+		t.FailNow()
+		return
 	}
 
-	t.Run("Actual input", func(t *testing.T) {
-		day02 := &d02.Day02{}
-		err := day02.LoadInput(log, actual)
-		assert.NoError(t, err)
-		output := day02.Part1(log)
-		fmt.Printf("\nPart 1:\n%d\n", output)
+	t.Run("Part 2", func(t *testing.T) {
+		if len(sample) > 0 {
+			ok := t.Run("1 Test case", func(t *testing.T) {
+				day02 := &d02.Day02{}
+				err := day02.LoadInput(log, sample)
+				assert.NoError(t, err)
+				output := day02.Part2(log)
 
-		assert.EqualValues(t, 2073315, output)
-	})
-}
+				assert.EqualValues(t, 900, output)
+			})
+			if !ok {
+				t.FailNow()
+				return
+			}
+		}
 
-func Test_Part2(t *testing.T) {
-	log, sample, actual := util.SetupTest(t, inputs, "02")
-
-	if len(sample) > 0 {
-		ok := t.Run("Test case", func(t *testing.T) {
+		t.Run("2 Actual input", func(t *testing.T) {
 			day02 := &d02.Day02{}
-			err := day02.LoadInput(log, sample)
+			err := day02.LoadInput(log, actual)
 			assert.NoError(t, err)
 			output := day02.Part2(log)
+			fmt.Print("=================================\n\n")
+			fmt.Printf("Part 2:\n%d", output)
+			fmt.Print("\n\n=================================\n")
 
-			assert.EqualValues(t, 900, output)
+			assert.EqualValues(t, 1840311528, output)
 		})
-		require.True(t, ok)
-	}
-
-	t.Run("Actual input", func(t *testing.T) {
-		day02 := &d02.Day02{}
-		err := day02.LoadInput(log, actual)
-		assert.NoError(t, err)
-		output := day02.Part2(log)
-		fmt.Printf("\nPart 2:\n%d\n", output)
-
-		assert.EqualValues(t, 1840311528, output)
 	})
 }
