eeea98a47ded2db93109fafa6073e5c55cf6f391
Sumner Evans <me@sumnerevans.com>
2022-11-30 22:47:19 -0700

    2022/01: really cleanup
    Signed-off-by: Sumner Evans <me@sumnerevans.com>

--- y2022/d01/01.go
+++ y2022/d01/01.go
@@ -4,36 +4,29 @@ import (
 	"github.com/rs/zerolog"
 	"github.com/sumnerevans/advent-of-code/lib"
 	"github.com/sumnerevans/advent-of-code/lib/fp"
-	math "github.com/sumnerevans/advent-of-code/lib/math"
 )
 
 type Day01 struct {
-	G [][]int64
+	GroupSums []int64
 }
 
 func (d *Day01) LoadInput(log *zerolog.Logger, lines []string) (err error) {
-	cur := []int64{}
+	var sum int64
 	for _, line := range lines {
 		if line == "" {
-			d.G = append(d.G, cur)
-			cur = []int64{}
+			d.GroupSums = append(d.GroupSums, sum)
+			sum = 0
 		} else {
 			x, _ := lib.ToInt64(line)
-			cur = append(cur, x)
+			sum += x
 		}
 	}
-	d.G = append(d.G, cur)
+	d.GroupSums = append(d.GroupSums, sum)
 	return err
 }
 
 func (d *Day01) Part1(log *zerolog.Logger) int64 {
-	var ans int64
-
-	for _, nums := range d.G {
-		ans = math.Max(fp.Sum(nums), ans)
-	}
-
-	return ans
+	return fp.Sum(lib.TopN(d.GroupSums, 1))
 }
 
 func (d *Day01) SkipFirst() bool {
@@ -41,11 +34,5 @@ func (d *Day01) SkipFirst() bool {
 }
 
 func (d *Day01) Part2(log *zerolog.Logger) int64 {
-	var groupSums []int64
-
-	for _, nums := range d.G {
-		groupSums = append(groupSums, fp.Sum(nums))
-	}
-
-	return fp.Sum(lib.TopN(groupSums, 3))
+	return fp.Sum(lib.TopN(d.GroupSums, 3))
 }
