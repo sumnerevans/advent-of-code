cb8b939a11340562c8ad078a7aaed5a65b194c33
Sumner Evans <me@sumnerevans.com>
2022-12-16 11:14:26 -0700

    2022/16: convert opens to bit representation
    Signed-off-by: Sumner Evans <me@sumnerevans.com>

--- lib/bits.go
+++ lib/bits.go
@@ -1,6 +1,22 @@
 package lib
 
+import (
+	"strings"
+)
+
 type Bits []bool
+
+func (b Bits) String() string {
+	var builder strings.Builder
+	for _, bit := range b {
+		if bit {
+			builder.WriteRune('1')
+		} else {
+			builder.WriteRune('0')
+		}
+	}
+	return builder.String()
+}
 
 func (b Bits) AsInt() int64 {
 	var val int64 = 0
@@ -11,4 +27,24 @@ func (b Bits) AsInt() int64 {
 		}
 	}
 	return val
+}
+
+func BitsOfInt(input int) Bits {
+	bits := make([]bool, 32)
+	for i := 0; i < 32; i++ {
+		if input&(1<<i) > 0 {
+			bits[i] = true
+		}
+	}
+	return bits
+}
+
+func BitsOfInt64(input int64) Bits {
+	bits := make([]bool, 64)
+	for i := 0; i < 64; i++ {
+		if input&(1<<i) > 0 {
+			bits[i] = true
+		}
+	}
+	return bits
 }
--- y2022/d16/16.go
+++ y2022/d16/16.go
@@ -14,12 +14,13 @@ type Valve struct {
 }
 
 type Day16 struct {
-	Valves    map[string]Valve
-	Distances map[string]map[string]int
+	Valves     map[string]Valve
+	ValveMasks map[string]int64
+	Distances  map[string]map[string]int
 }
 
 func (d *Day16) LoadInput(lines []string) error {
-	valves := map[string]Valve{}
+	d.Valves = map[string]Valve{}
 	for _, line := range lines {
 		grps := lib.ReGroups(`Valve (\w+) has flow rate=(\d+); tunnels? leads? to valves? (.*)`, line)
 		name := grps[0]
@@ -28,11 +29,11 @@ func (d *Day16) LoadInput(lines []string) error {
 		for _, x := range strings.Split(grps[2], ", ") {
 			adj.Add(ds.Edge[string, int]{1, x})
 		}
-		valves[name] = Valve{Flow: flow, Adj: adj}
+		d.Valves[name] = Valve{Flow: flow, Adj: adj}
 	}
 
 	d.Distances = map[string]map[string]int{}
-	for name := range valves {
+	for name := range d.Valves {
 		d.Distances[name] = map[string]int{}
 		pq := ds.NewPriorityQueue(ds.NewPair(0, name))
 		seen := ds.Set[string]{}
@@ -42,7 +43,7 @@ func (d *Day16) LoadInput(lines []string) error {
 			if _, ok := d.Distances[name][el]; !ok {
 				d.Distances[name][el] = cost
 			}
-			for a := range valves[el].Adj {
+			for a := range d.Valves[el].Adj {
 				if !seen.Contains(a.Vertex) {
 					pq.Push(cost+1, a.Vertex)
 				}
@@ -51,7 +52,17 @@ func (d *Day16) LoadInput(lines []string) error {
 	}
 	fmt.Printf("dist %v\n", d.Distances)
 
-	panic(1)
+	d.ValveMasks = map[string]int64{}
+	i := 0
+	for name := range d.Valves {
+		d.ValveMasks[name] = 1 << i
+		i++
+	}
+	fmt.Printf("VM %v\n", d.ValveMasks)
+	for k, v := range d.ValveMasks {
+		fmt.Printf("k: %s\n", k)
+		fmt.Printf("v: %v\n", lib.BitsOfInt64(v).String())
+	}
 
 	// 	handled := ds.Set[string]{}
 	// 	for {
@@ -93,628 +104,48 @@ func (d *Day16) LoadInput(lines []string) error {
 	return nil
 }
 
-type CurOpens struct {
-	ValveAA bool
-	ValveBB bool
-	ValveCC bool
-	ValveDD bool
-	ValveEE bool
-	ValveFF bool
-	ValveGG bool
-	ValveHH bool
-	ValveII bool
-	ValveJJ bool
+type CurOpens int64
 
-	ValveAD bool
-	ValveAN bool
-	ValveBU bool
-	ValveBV bool
-	ValveCK bool
-	ValveDS bool
-	ValveDV bool
-	ValveDW bool
-	ValveEG bool
-	ValveEL bool
-	ValveEV bool
-	ValveFB bool
-	ValveFE bool
-	ValveFT bool
-	ValveGC bool
-	ValveGE bool
-	ValveGI bool
-	ValveGS bool
-	ValveGV bool
-	ValveHB bool
-	ValveHI bool
-	ValveHO bool
-	ValveHS bool
-	ValveIQ bool
-	ValveJA bool
-	ValveJG bool
-	ValveJH bool
-	ValveJV bool
-	ValveKS bool
-	ValveKU bool
-	ValveLS bool
-	ValveLT bool
-	ValveNK bool
-	ValveNY bool
-	ValveOI bool
-	ValveQU bool
-	ValveRD bool
-	ValveRM bool
-	ValveSD bool
-	ValveSS bool
-	ValveSV bool
-	ValveTC bool
-	ValveTJ bool
-	ValveTV bool
-	ValveUU bool
-	ValveUY bool
-	ValveVA bool
-	ValveWB bool
-	ValveWI bool
-	ValveWJ bool
-	ValveYC bool
-	ValveYP bool
-	ValveYZ bool
-	ValveZH bool
-	ValveZN bool
-	ValveZV bool
+func (co CurOpens) IsOpen(mask int64) bool {
+	return int64(co)&mask != 0
 }
 
-func (co CurOpens) IsOpen(v string) bool {
-	switch v {
-
-	case "AA":
-		return co.ValveAA
-	case "BB":
-		return co.ValveBB
-	case "CC":
-		return co.ValveCC
-	case "DD":
-		return co.ValveDD
-	case "EE":
-		return co.ValveEE
-	case "FF":
-		return co.ValveFF
-	case "GG":
-		return co.ValveGG
-	case "HH":
-		return co.ValveHH
-	case "II":
-		return co.ValveII
-	case "JJ":
-		return co.ValveJJ
-
-	case "AD":
-		return co.ValveAD
-	case "AN":
-		return co.ValveAN
-	case "BU":
-		return co.ValveBU
-	case "BV":
-		return co.ValveBV
-	case "CK":
-		return co.ValveCK
-	case "DS":
-		return co.ValveDS
-	case "DV":
-		return co.ValveDV
-	case "DW":
-		return co.ValveDW
-	case "EG":
-		return co.ValveEG
-	case "EL":
-		return co.ValveEL
-	case "EV":
-		return co.ValveEV
-	case "FB":
-		return co.ValveFB
-	case "FE":
-		return co.ValveFE
-	case "FT":
-		return co.ValveFT
-	case "GC":
-		return co.ValveGC
-	case "GE":
-		return co.ValveGE
-	case "GI":
-		return co.ValveGI
-	case "GS":
-		return co.ValveGS
-	case "GV":
-		return co.ValveGV
-	case "HB":
-		return co.ValveHB
-	case "HI":
-		return co.ValveHI
-	case "HO":
-		return co.ValveHO
-	case "HS":
-		return co.ValveHS
-	case "IQ":
-		return co.ValveIQ
-	case "JA":
-		return co.ValveJA
-	case "JG":
-		return co.ValveJG
-	case "JH":
-		return co.ValveJH
-	case "JV":
-		return co.ValveJV
-	case "KS":
-		return co.ValveKS
-	case "KU":
-		return co.ValveKU
-	case "LS":
-		return co.ValveLS
-	case "LT":
-		return co.ValveLT
-	case "NK":
-		return co.ValveNK
-	case "NY":
-		return co.ValveNY
-	case "OI":
-		return co.ValveOI
-	case "QU":
-		return co.ValveQU
-	case "RD":
-		return co.ValveRD
-	case "RM":
-		return co.ValveRM
-	case "SD":
-		return co.ValveSD
-	case "SS":
-		return co.ValveSS
-	case "SV":
-		return co.ValveSV
-	case "TC":
-		return co.ValveTC
-	case "TJ":
-		return co.ValveTJ
-	case "TV":
-		return co.ValveTV
-	case "UU":
-		return co.ValveUU
-	case "UY":
-		return co.ValveUY
-	case "VA":
-		return co.ValveVA
-	case "WB":
-		return co.ValveWB
-	case "WI":
-		return co.ValveWI
-	case "WJ":
-		return co.ValveWJ
-	case "YC":
-		return co.ValveYC
-	case "YP":
-		return co.ValveYP
-	case "YZ":
-		return co.ValveYZ
-	case "ZH":
-		return co.ValveZH
-	case "ZN":
-		return co.ValveZN
-	case "ZV":
-		return co.ValveZV
-	default:
-		panic(v)
+func (co CurOpens) TotalFlow(maskMap map[string]int64, flows map[string]Valve) int {
+	maskToName := map[int64]string{}
+	for k, v := range maskMap {
+		maskToName[v] = k
 	}
+	var i int64
+	total := 0
+	for ; i < 64; i++ {
+		if co.IsOpen(i) {
+			total += flows[maskToName[i]].Flow
+		}
+	}
+	return total
 }
 
-// func (co CurOpens) AllOpen() bool {
-// 	return (co.ValveAA &&
-// 		co.ValveBB &&
-// 		co.ValveCC &&
-// 		co.ValveDD &&
-// 		co.ValveEE &&
-// 		co.ValveFF &&
-// 		co.ValveGG &&
-// 		co.ValveHH &&
-// 		co.ValveII &&
-// 		co.ValveJJ) || (co.ValveAA &&
-// 		co.ValveAD &&
-// 		co.ValveAN &&
-// 		co.ValveBU &&
-// 		co.ValveBV &&
-// 		co.ValveCK &&
-// 		co.ValveDS &&
-// 		co.ValveDV &&
-// 		co.ValveDW &&
-// 		co.ValveEG &&
-// 		co.ValveEL &&
-// 		co.ValveEV &&
-// 		co.ValveFB &&
-// 		co.ValveFE &&
-// 		co.ValveFT &&
-// 		co.ValveGC &&
-// 		co.ValveGE &&
-// 		co.ValveGI &&
-// 		co.ValveGS &&
-// 		co.ValveGV &&
-// 		co.ValveHB &&
-// 		co.ValveHI &&
-// 		co.ValveHO &&
-// 		co.ValveHS &&
-// 		co.ValveIQ &&
-// 		co.ValveJA &&
-// 		co.ValveJG &&
-// 		co.ValveJH &&
-// 		co.ValveJV &&
-// 		co.ValveKS &&
-// 		co.ValveKU &&
-// 		co.ValveLS &&
-// 		co.ValveLT &&
-// 		co.ValveNK &&
-// 		co.ValveNY &&
-// 		co.ValveOI &&
-// 		co.ValveQU &&
-// 		co.ValveRD &&
-// 		co.ValveRM &&
-// 		co.ValveSD &&
-// 		co.ValveSS &&
-// 		co.ValveSV &&
-// 		co.ValveTC &&
-// 		co.ValveTJ &&
-// 		co.ValveTV &&
-// 		co.ValveUU &&
-// 		co.ValveUY &&
-// 		co.ValveVA &&
-// 		co.ValveWB &&
-// 		co.ValveWI &&
-// 		co.ValveWJ &&
-// 		co.ValveYC &&
-// 		co.ValveYP &&
-// 		co.ValveYZ &&
-// 		co.ValveZH &&
-// 		co.ValveZN &&
-// 		co.ValveZV)
-// }
-
-func (co CurOpens) TotalFlow(flows map[string]Valve) int {
-	sum := 0
-	if co.ValveAA {
-		sum += flows["AA"].Flow
-	}
-	if co.ValveBB {
-		sum += flows["BB"].Flow
-	}
-	if co.ValveCC {
-		sum += flows["CC"].Flow
-	}
-	if co.ValveDD {
-		sum += flows["DD"].Flow
-	}
-	if co.ValveEE {
-		sum += flows["EE"].Flow
-	}
-	if co.ValveFF {
-		sum += flows["FF"].Flow
-	}
-	if co.ValveGG {
-		sum += flows["GG"].Flow
-	}
-	if co.ValveHH {
-		sum += flows["HH"].Flow
-	}
-	if co.ValveII {
-		sum += flows["II"].Flow
-	}
-	if co.ValveJJ {
-		sum += flows["JJ"].Flow
-	}
-
-	if co.ValveAD {
-		sum += flows["AD"].Flow
-	}
-	if co.ValveAN {
-		sum += flows["AN"].Flow
-	}
-	if co.ValveBU {
-		sum += flows["BU"].Flow
-	}
-	if co.ValveBV {
-		sum += flows["BV"].Flow
-	}
-	if co.ValveCK {
-		sum += flows["CK"].Flow
-	}
-	if co.ValveDS {
-		sum += flows["DS"].Flow
-	}
-	if co.ValveDV {
-		sum += flows["DV"].Flow
-	}
-	if co.ValveDW {
-		sum += flows["DW"].Flow
-	}
-	if co.ValveEG {
-		sum += flows["EG"].Flow
-	}
-	if co.ValveEL {
-		sum += flows["EL"].Flow
-	}
-	if co.ValveEV {
-		sum += flows["EV"].Flow
-	}
-	if co.ValveFB {
-		sum += flows["FB"].Flow
-	}
-	if co.ValveFE {
-		sum += flows["FE"].Flow
-	}
-	if co.ValveFT {
-		sum += flows["FT"].Flow
-	}
-	if co.ValveGC {
-		sum += flows["GC"].Flow
-	}
-	if co.ValveGE {
-		sum += flows["GE"].Flow
-	}
-	if co.ValveGI {
-		sum += flows["GI"].Flow
-	}
-	if co.ValveGS {
-		sum += flows["GS"].Flow
-	}
-	if co.ValveGV {
-		sum += flows["GV"].Flow
-	}
-
-	if co.ValveHB {
-		sum += flows["HB"].Flow
-	}
-	if co.ValveHI {
-		sum += flows["HI"].Flow
-	}
-	if co.ValveHO {
-		sum += flows["HO"].Flow
-	}
-	if co.ValveHS {
-		sum += flows["HS"].Flow
-	}
-	if co.ValveIQ {
-		sum += flows["IQ"].Flow
-	}
-	if co.ValveJA {
-		sum += flows["JA"].Flow
-	}
-	if co.ValveJG {
-		sum += flows["JG"].Flow
-	}
-	if co.ValveJH {
-		sum += flows["JH"].Flow
-	}
-	if co.ValveJV {
-		sum += flows["JV"].Flow
-	}
-	if co.ValveKS {
-		sum += flows["KS"].Flow
-	}
-	if co.ValveKU {
-		sum += flows["KU"].Flow
-	}
-	if co.ValveLS {
-		sum += flows["LS"].Flow
-	}
-	if co.ValveLT {
-		sum += flows["LT"].Flow
-	}
-	if co.ValveNK {
-		sum += flows["NK"].Flow
-	}
-	if co.ValveNY {
-		sum += flows["NY"].Flow
-	}
-	if co.ValveOI {
-		sum += flows["OI"].Flow
-	}
-	if co.ValveQU {
-		sum += flows["QU"].Flow
-	}
-	if co.ValveRD {
-		sum += flows["RD"].Flow
-	}
-	if co.ValveRM {
-		sum += flows["RM"].Flow
-	}
-	if co.ValveSD {
-		sum += flows["SD"].Flow
-	}
-	if co.ValveSS {
-		sum += flows["SS"].Flow
-	}
-	if co.ValveSV {
-		sum += flows["SV"].Flow
-	}
-	if co.ValveTC {
-		sum += flows["TC"].Flow
-	}
-	if co.ValveTJ {
-		sum += flows["TJ"].Flow
-	}
-	if co.ValveTV {
-		sum += flows["TV"].Flow
-	}
-	if co.ValveUU {
-		sum += flows["UU"].Flow
-	}
-	if co.ValveUY {
-		sum += flows["UY"].Flow
-	}
-	if co.ValveVA {
-		sum += flows["VA"].Flow
-	}
-	if co.ValveWB {
-		sum += flows["WB"].Flow
-	}
-	if co.ValveWI {
-		sum += flows["WI"].Flow
-	}
-	if co.ValveWJ {
-		sum += flows["WJ"].Flow
-	}
-	if co.ValveYC {
-		sum += flows["YC"].Flow
-	}
-	if co.ValveYP {
-		sum += flows["YP"].Flow
-	}
-	if co.ValveYZ {
-		sum += flows["YZ"].Flow
-	}
-	if co.ValveZH {
-		sum += flows["ZH"].Flow
-	}
-	if co.ValveZN {
-		sum += flows["ZN"].Flow
-	}
-	if co.ValveZV {
-		sum += flows["ZV"].Flow
-	}
-	return sum
+func (co CurOpens) WithOpen(mask int64) CurOpens {
+	if co.IsOpen(mask) {
+		panic("already open")
+	}
+	return CurOpens(int64(co) | mask)
 }
 
-func (co CurOpens) WithOpen(v string) CurOpens {
-	switch v {
-	case "AA":
-		co.ValveAA = true
-	case "BB":
-		co.ValveBB = true
-	case "CC":
-		co.ValveCC = true
-	case "DD":
-		co.ValveDD = true
-	case "EE":
-		co.ValveEE = true
-	case "FF":
-		co.ValveFF = true
-	case "GG":
-		co.ValveGG = true
-	case "HH":
-		co.ValveHH = true
-	case "II":
-		co.ValveII = true
-	case "JJ":
-		co.ValveJJ = true
-
-	case "AD":
-		co.ValveAD = true
-	case "AN":
-		co.ValveAN = true
-	case "BU":
-		co.ValveBU = true
-	case "BV":
-		co.ValveBV = true
-	case "CK":
-		co.ValveCK = true
-	case "DS":
-		co.ValveDS = true
-	case "DV":
-		co.ValveDV = true
-	case "DW":
-		co.ValveDW = true
-	case "EG":
-		co.ValveEG = true
-	case "EL":
-		co.ValveEL = true
-	case "EV":
-		co.ValveEV = true
-	case "FB":
-		co.ValveFB = true
-	case "FE":
-		co.ValveFE = true
-	case "FT":
-		co.ValveFT = true
-	case "GC":
-		co.ValveGC = true
-	case "GE":
-		co.ValveGE = true
-	case "GI":
-		co.ValveGI = true
-	case "GS":
-		co.ValveGS = true
-	case "GV":
-		co.ValveGV = true
-	case "HB":
-		co.ValveHB = true
-	case "HI":
-		co.ValveHI = true
-	case "HO":
-		co.ValveHO = true
-	case "HS":
-		co.ValveHS = true
-	case "IQ":
-		co.ValveIQ = true
-	case "JA":
-		co.ValveJA = true
-	case "JG":
-		co.ValveJG = true
-	case "JH":
-		co.ValveJH = true
-	case "JV":
-		co.ValveJV = true
-	case "KS":
-		co.ValveKS = true
-	case "KU":
-		co.ValveKU = true
-	case "LS":
-		co.ValveLS = true
-	case "LT":
-		co.ValveLT = true
-	case "NK":
-		co.ValveNK = true
-	case "NY":
-		co.ValveNY = true
-	case "OI":
-		co.ValveOI = true
-	case "QU":
-		co.ValveQU = true
-	case "RD":
-		co.ValveRD = true
-	case "RM":
-		co.ValveRM = true
-	case "SD":
-		co.ValveSD = true
-	case "SS":
-		co.ValveSS = true
-	case "SV":
-		co.ValveSV = true
-	case "TC":
-		co.ValveTC = true
-	case "TJ":
-		co.ValveTJ = true
-	case "TV":
-		co.ValveTV = true
-	case "UU":
-		co.ValveUU = true
-	case "UY":
-		co.ValveUY = true
-	case "VA":
-		co.ValveVA = true
-	case "WB":
-		co.ValveWB = true
-	case "WI":
-		co.ValveWI = true
-	case "WJ":
-		co.ValveWJ = true
-	case "YC":
-		co.ValveYC = true
-	case "YP":
-		co.ValveYP = true
-	case "YZ":
-		co.ValveYZ = true
-	case "ZH":
-		co.ValveZH = true
-	case "ZN":
-		co.ValveZN = true
-	case "ZV":
-		co.ValveZV = true
+func (co CurOpens) Opens(maskMap map[string]int64) []string {
+	maskToName := map[int64]string{}
+	for k, v := range maskMap {
+		maskToName[v] = k
 	}
-	return co
+
+	var i int64
+	opens := []string{}
+	for ; i < 64; i++ {
+		if co.IsOpen(i) {
+			opens = append(opens, maskToName[i])
+		}
+	}
+	return opens
 }
 
 type CurState struct {
@@ -724,8 +155,51 @@ type CurState struct {
 	Flow   int
 }
 
-// This isn't really BFS, I have no idea what it is
-func BFS(
+type Cons[T any] struct {
+	Car T
+	Cdr *Cons[T]
+}
+
+type Stack[T any] struct {
+	head *Cons[T]
+}
+
+func (s *Stack[T]) Len() int {
+	i := 0
+	cur := s.head
+	for cur != nil {
+		i++
+		cur = cur.Cdr
+	}
+	return i
+}
+
+func (s *Stack[T]) Push(x T) {
+	if s.head == nil {
+		s.head = &Cons[T]{Car: x}
+	}
+}
+
+func (s *Stack[T]) Peek() *T {
+	if s.head == nil {
+		return nil
+	}
+	return &s.head.Car
+}
+
+func (s *Stack[T]) Pop() T {
+	if s.head == nil {
+		panic("pop from empty stack")
+	}
+
+	top := s.head.Car
+	s.head = s.head.Cdr
+	return top
+}
+
+// This isn't really DFS, I have no idea what it is.
+func DFS(
+	d *Day16,
 	nextStates func(CurState) ds.Set[CurState],
 	start CurState,
 	endState func(CurState) bool,
@@ -733,18 +207,21 @@ func BFS(
 ) int {
 	// endStates := []CurState{}
 
-	pq := ds.NewPriorityQueue(ds.NewPair(0, start))
+	stack := Stack[CurState]{}
+	stack.Push(start)
+	// pq := ds.NewPriorityQueue(ds.NewPair(0, start))
 	// seen := ds.Set[CurState]{}
 
 	i := 0
 	best := 0
 
-	for pq.Len() > 0 {
+	for stack.Peek() != nil {
 		if i%10000000 == 0 {
-			fmt.Printf("%d %d\n", pq.Len(), i)
+			fmt.Printf("%d %d\n", stack.Len(), i)
 		}
 		i++
-		_, el := pq.Pop()
+		el := stack.Pop()
+		fmt.Printf("el %v\n", el.Open.Opens(d.ValveMasks))
 		// if seen.Contains(el) {
 		// 	continue
 		// }
@@ -774,7 +251,7 @@ func BFS(
 			// if seen.Contains(e) {
 			// 	continue
 			// }
-			pq.Push(0, e)
+			stack.Push(e)
 		}
 	}
 
@@ -784,8 +261,17 @@ func BFS(
 func (d *Day16) Part1(isTest bool) int {
 	var ans int
 
+	// valveMasks := map[string]int64{}
+	// i := 0
+	// for name := range d.Valves {
+	// 	valveMasks[name] = 1 << i
+	// 	i++
+	// }
+	// fmt.Printf("%v\n", valveMasks)
+	// panic(1)
+
 	start := CurState{
-		Open:   CurOpens{},
+		Open:   0,
 		CurPos: "AA",
 		Time:   1,
 		Flow:   0,
@@ -795,19 +281,20 @@ func (d *Day16) Part1(isTest bool) int {
 		return v.Flow
 	})(lib.Values(d.Valves)))
 
-	return BFS(
+	return DFS(
+		d,
 		func(cur CurState) ds.Set[CurState] {
 			// fmt.Printf("cur %v\n", cur)
 			next := ds.Set[CurState]{}
 
 			// Open the valve
-			if !cur.Open.IsOpen(cur.CurPos) {
+			if !cur.Open.IsOpen(d.ValveMasks[cur.CurPos]) {
 				next.Add(
 					CurState{
-						Open:   cur.Open.WithOpen(cur.CurPos),
+						Open:   cur.Open.WithOpen(d.ValveMasks[cur.CurPos]),
 						CurPos: cur.CurPos,
 						Time:   cur.Time + 1,
-						Flow:   cur.Flow + cur.Open.TotalFlow(d.Valves),
+						Flow:   cur.Flow + cur.Open.TotalFlow(d.ValveMasks, d.Valves),
 					},
 				)
 			}
@@ -820,7 +307,7 @@ func (d *Day16) Part1(isTest bool) int {
 							Open:   cur.Open,
 							CurPos: v.Vertex,
 							Time:   cur.Time + v.Weight,
-							Flow:   cur.Flow + cur.Open.TotalFlow(d.Valves),
+							Flow:   cur.Flow + cur.Open.TotalFlow(d.ValveMasks, d.Valves),
 						},
 					)
 				}
@@ -832,7 +319,7 @@ func (d *Day16) Part1(isTest bool) int {
 					Open:   cur.Open,
 					CurPos: cur.CurPos,
 					Time:   cur.Time + 1,
-					Flow:   cur.Flow + cur.Open.TotalFlow(d.Valves),
+					Flow:   cur.Flow + cur.Open.TotalFlow(d.ValveMasks, d.Valves),
 				},
 			)
 			return next
