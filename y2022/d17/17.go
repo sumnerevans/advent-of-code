d3459da05ba2e7384836fe11b2773670f5af92db
Sumner Evans <me@sumnerevans.com>
2022-12-17 23:11:09 -0700

    2022/17: cleanup
    Signed-off-by: Sumner Evans <me@sumnerevans.com>

--- y2022/d17/17.go
+++ y2022/d17/17.go
@@ -156,25 +156,11 @@ func (d *Day17) Solve(iters int64) int64 {
 			if perShape, ok := floorShapePerIdx[currentFloorShape]; ok {
 				if perDir, ok := perShape[curShapeIdx]; ok {
 					if prevIdx, ok := perDir[curDirIdx]; ok {
-						fmt.Printf("GOT HERE %d %d\n", prevIdx, i)
-						fmt.Printf("%v\n", currentFloorShape)
-						fmt.Printf("%v\n", shapes[curShapeIdx])
-						fmt.Printf("%v\n", d.Dirs[curDirIdx])
-						fmt.Printf("curtop %d\n", top)
-						fmt.Printf("prvtop %d\n", prevIdx.Top)
-						fmt.Printf("curidx %d\n", i)
-						fmt.Printf("prvidx %d\n", prevIdx.Idx)
-
 						di := i - prevIdx.Idx
-						fmt.Printf("di %d\n", di)
-						fmt.Printf("i+di %d %d\n", i+di, iters)
 						if i+di < iters {
 							cycleCount := (iters - i) / di
-							fmt.Printf("cycles %d\n", cycleCount)
-							fmt.Printf("dt %d\n", top-prevIdx.Top)
 							top += cycleCount * (top - prevIdx.Top)
 							i += cycleCount*di - 1 // the -1 here is because we re-add it in the for-loop
-							fmt.Printf("NEW i=%d\n", i)
 							foundCycle = true
 							continue
 						}
@@ -183,11 +169,8 @@ func (d *Day17) Solve(iters int64) int64 {
 			}
 		}
 
-		// fmt.Printf("Unknown combination of floor shape and sequence index\n")
-
 		shape := shapes[curShapeIdx]
 
-		// fmt.Printf("cfs %v\n", currentFloorShape)
 		chamber := ds.Set[lib.GridPoint[int64]]{}
 		for dr := 0; dr < ROWS; dr++ {
 			for c := 0; c < 7; c++ {
@@ -200,14 +183,6 @@ func (d *Day17) Solve(iters int64) int64 {
 		shapeBottomLeftPos := lib.GridPoint[int64]{R: top + 3, C: 2}
 		newDirIdx := curDirIdx
 		for {
-			// fmt.Printf("x %d\n", shape)
-			// fmt.Printf("START\n")
-			// fmt.Printf("\n")
-			if shapeBottomLeftPos.R < top-int64(ROWS) {
-				PrintChamberWithShape(chamber, shape, shapeBottomLeftPos)
-				panic("falling!")
-			}
-
 			// Direction
 			var newPos lib.GridPoint[int64]
 			switch d.Dirs[newDirIdx] {
@@ -219,33 +194,19 @@ func (d *Day17) Solve(iters int64) int64 {
 				panic("fail")
 			}
 			newDirIdx = (newDirIdx + 1) % len(d.Dirs)
-			// fmt.Printf("DIR %v\n", shapeBottomLeftPos)
-			// fmt.Printf("%v %v %v\n", !shape.Intersects(newPos, chamber), newPos.C >= 0, newPos.C+len(shape[0]) <= 7)
 			if !shape.Intersects(newPos, chamber) && newPos.C >= 0 && newPos.C+int64(len(shape[0])) <= 7 {
-				// fmt.Printf("Rock moves %d horizontally\n", int(d.Dirs[x%len(d.Dirs)]))
-				// fmt.Printf("--> %v\n", newPos)
 				shapeBottomLeftPos = newPos
-			} else {
-				// fmt.Printf("Rock doesn't move horizontally\n")
 			}
-			// PrintChamberWithShape(chamber, shape, shapeBottomLeftPos)
 
-			// fmt.Printf("Rock falls one unit\n")
 			// Down
 			newPos = lib.GridPoint[int64]{R: shapeBottomLeftPos.R - 1, C: shapeBottomLeftPos.C}
-			// fmt.Printf("%v %v\n", chamber, newPos)
-			// if newPos.R < -10{panic("ohea")}
 			if shape.Intersects(newPos, chamber) {
-				// fmt.Printf("... and comes to rest\n")
 				break
 			} else {
-				// fmt.Printf("-> %v\n", newPos)
 				shapeBottomLeftPos = newPos
 			}
-			// PrintChamberWithShape(chamber, shape, shapeBottomLeftPos)
 		}
 
-		// fmt.Printf("PLACE %v\n", shapeBottomLeftPos)
 		for r := 0; r < len(shape); r++ {
 			for c := 0; c < len(shape[0]); c++ {
 				if shape[r][c] {
@@ -261,19 +222,6 @@ func (d *Day17) Solve(iters int64) int64 {
 			return gp.R + 1
 		})
 
-		// fmt.Printf("chamber %v\n", chamber)
-		// for r := top; r >= 0; r-- {
-		// 	fmt.Printf("|")
-		// 	for c := 0; c < 7; c++ {
-		// 		if chamber.Contains(lib.GridPoint[int]{R: r, C: c}) {
-		// 			fmt.Printf("#")
-		// 		} else {
-		// 			fmt.Printf(".")
-		// 		}
-		// 	}
-		// 	fmt.Printf("|\n")
-		// }
-
 		newFloorShape := FloorShape{}
 		for dr := 0; dr < ROWS; dr++ {
 			for c := 0; c < 7; c++ {
@@ -282,11 +230,6 @@ func (d *Day17) Solve(iters int64) int64 {
 				}
 			}
 		}
-		// if newFloorShape.Space1.Cmp(big.NewInt(0)) == 0 || newFloorShape.Space2.Cmp(big.NewInt(0)) == 0 || newFloorShape.Space3.Cmp(big.NewInt(0)) == 0 || newFloorShape.Space4.Cmp(big.NewInt(0)) == 0 || newFloorShape.Space5.Cmp(big.NewInt(0)) == 0 || newFloorShape.Space6.Cmp(big.NewInt(0)) == 0 || newFloorShape.Space7.Cmp(big.NewInt(0)) == 0 {
-		// 	fmt.Printf("%v\n", newFloorShape)
-		// 	panic("something bad happened!")
-		// }
-		// fmt.Printf("%v\n", newFloorShape)
 		if _, ok := floorShapePerIdx[currentFloorShape]; !ok {
 			floorShapePerIdx[currentFloorShape] = map[int]map[int]ShapeHeight{}
 		}
@@ -299,11 +242,6 @@ func (d *Day17) Solve(iters int64) int64 {
 		curShapeIdx = (curShapeIdx + 1) % 5
 		curDirIdx = newDirIdx
 		top = newTop
-
-		// fmt.Printf("%v\n", floorShapePerIdx)
-		// if i >= 2 {
-		// 	break
-		// }
 	}
 
 	return top
