0feafba2bda6c3011b3f5910381be7271c24b4a8
Sumner Evans <me@sumnerevans.com>
2022-12-25 00:33:29 -0700

    2022/25: solved
    Signed-off-by: Sumner Evans <me@sumnerevans.com>

---
+++ y2022/d25/25.go
@@ -0,0 +1,108 @@
+package d25
+
+import (
+	"fmt"
+	"math"
+)
+
+type SNAFUNum string
+
+// "You know, I never did ask the engineers why they did that. Instead of using
+// digits four through zero, the digits are 2, 1, 0, minus (written -), and
+// double-minus (written =). Minus is worth -1, and double-minus is worth -2."
+
+func (s SNAFUNum) ToInt() int64 {
+	var pmult int64 = 1
+	var res int64
+	for place := len(s) - 1; place >= 0; place-- {
+		switch s[place] {
+		case '2':
+			res += pmult * 2
+		case '1':
+			res += pmult
+		case '0':
+		case '-':
+			res -= pmult
+		case '=':
+			res -= 2 * pmult
+		}
+
+		pmult *= 5
+	}
+	return res
+}
+
+type Day25 struct {
+	Nums []SNAFUNum
+}
+
+func Pow(base, exp int64) int64 {
+	return int64(math.Pow(float64(base), float64(exp)))
+}
+
+func (d *Day25) LoadInput(lines []string) error {
+	for _, line := range lines {
+		d.Nums = append(d.Nums, SNAFUNum(line))
+	}
+	return nil
+}
+
+func (d *Day25) Part1(isTest bool) string {
+	var sum int64
+
+
+	for _, s := range d.Nums {
+		sum += s.ToInt()
+	}
+
+	fmt.Printf("SUM=%d\n", sum)
+
+	var hipow int64 = 1
+	for Pow(5, int64(hipow))*2 < sum {
+		hipow *= 5
+	}
+	fmt.Printf("hipow %d\n", hipow)
+
+	res := ""
+	numMap := map[int64]string{-2: "=", -1: "-", 0: "0", 1: "1", 2: "2"}
+
+	target := sum
+	for exp := hipow; exp >= 0; exp-- {
+		var m int64 = -3
+		var hi, lo int64
+
+		place := Pow(5, exp)
+		fmt.Printf("PLACE = %d\n", place)
+
+		for ; m <= 3; m++ {
+			// fmt.Printf("%d\n", place*m)
+			if place*m <= target {
+				lo = m
+			}
+			if place*m > target {
+				hi = m
+				break
+			}
+		}
+
+		fmt.Printf("t = %d\n", target)
+		fmt.Printf("p*lo = %d\n", place*lo)
+		fmt.Printf("p*hi = %d\n", place*hi)
+		if target-place*lo < place*hi-target {
+			fmt.Printf("lo closer\n")
+			res += numMap[lo]
+			target -= place*lo
+		} else {
+			res += numMap[hi]
+			target -= place*hi
+			fmt.Printf("hi closer\n")
+		}
+		fmt.Printf("RES = %s\n", res)
+	}
+
+	for res[0] == '0' {
+		res = res[1:]
+	}
+
+	return res
+}
---
+++ y2022/d25/25_test.go
@@ -0,0 +1,143 @@
+package d25_test
+
+import (
+	"embed"
+	"fmt"
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+
+	"github.com/sumnerevans/advent-of-code/lib"
+	"github.com/sumnerevans/advent-of-code/y2022/d25"
+)
+
+//go:embed *.txt
+var inputs embed.FS
+
+func Test_Day25(t *testing.T) {
+	t.Log(lib.ColorString("============================================", lib.ColorGreen))
+	t.Log(lib.ColorString("=                START TEST                =", lib.ColorGreen))
+	t.Log(lib.ColorString("============================================", lib.ColorGreen))
+
+	_, samples, actual := lib.SetupTest(t, inputs, "25")
+
+	ok := t.Run("Part 1", func(t *testing.T) {
+		if len(samples) > 0 {
+			ok := t.Run("1 Test cases", func(t *testing.T) {
+				EXPECTED := []string{
+					// Test cases
+					// "1=",
+					// "1-",
+					// "2=-1=0",
+				}
+
+				for i, sample := range samples {
+					if i >= len(EXPECTED) {
+						break
+					}
+
+					t.Run(fmt.Sprintf("Test %d", i+1), func(t *testing.T) {
+						day25 := &d25.Day25{}
+						err := day25.LoadInput(sample)
+						require.NoError(t, err)
+						output := day25.Part1(true)
+
+						assert.EqualValues(t, EXPECTED[i], output)
+					})
+				}
+			})
+			if !ok {
+				t.FailNow()
+				return
+			}
+		}
+
+		t.Run("2 Actual input", func(t *testing.T) {
+			day25 := &d25.Day25{}
+			err := day25.LoadInput(actual)
+			require.NoError(t, err)
+			output := day25.Part1(false)
+			t.Log("=================================")
+			t.Log("")
+			t.Log("ACTUAL INPUT")
+			t.Log("")
+			t.Log(lib.ColorString("Part 1:", lib.ColorBlue))
+			t.Log("")
+			t.Logf("%v", output)
+			t.Log("")
+
+			require.NotEqualValues(t, 0, output)
+
+			existingOutput, err := lib.ReadOutput(1)
+			if err != nil {
+				previousIncorrects, err := lib.ReadIncorrect(1)
+				require.NoError(t, err)
+				for _, prev := range previousIncorrects {
+					if prev == lib.AsJSON(output) {
+						t.Fatal(lib.ColorString("You already submitted that and it was incorrect\n", lib.ColorRed))
+					}
+				}
+
+				result, answerText := lib.Submit(t, 2022, 25, 1, output)
+				switch result {
+				case lib.SubmissionCorrect:
+					os.WriteFile("output.1.txt", []byte(lib.AsJSON(output)), 0644)
+					os.WriteFile("answertext.1.txt", []byte(answerText), 0644)
+
+					t.Log("=================================")
+					t.Log("")
+					t.Log(lib.ColorString("AUTOSUBMITTING PART 2 BECAUSE IT'S DAY 25", lib.ColorYellow))
+					t.Log("")
+
+					_, err := lib.ReadOutput(2)
+					if err != nil {
+						result, answerText := lib.Submit(t, 2022, 25, 2, "done")
+						switch result {
+						case lib.SubmissionCorrect:
+							os.WriteFile("output.2.txt", []byte(lib.AsJSON("done")), 0644)
+							os.WriteFile("answertext.2.txt", []byte(answerText), 0644)
+						default:
+							t.Fatal("Something bad happened with the autosubmission on part 2")
+						}
+					} else {
+						t.Log(lib.ColorString("Auto-submission already complete", lib.ColorGreen))
+						if answerText, err := os.ReadFile("answertext.2.txt"); err == nil {
+							t.Log("")
+							t.Log("Original server response:")
+							t.Log("")
+							for _, s := range lib.WrapString(string(answerText), 60) {
+								t.Log(lib.ColorString(s, lib.ColorGreen))
+							}
+						}
+					}
+				case lib.SubmissionIncorrect:
+					require.NoError(t, lib.WriteIncorrect(1, lib.AsJSON(output)))
+				case lib.SubmissionTooSoon:
+					t.Fatal(lib.ColorString("Submission was too recent.", lib.ColorRed))
+				}
+			} else if existingOutput == lib.AsJSON(output) {
+				t.Log(lib.ColorString("Answer already ACCEPTED", lib.ColorGreen))
+				if answerText, err := os.ReadFile("answertext.1.txt"); err == nil {
+					t.Log("")
+					t.Log("Original server response:")
+					t.Log("")
+					for _, s := range lib.WrapString(string(answerText), 60) {
+						t.Log(lib.ColorString(s, lib.ColorGreen))
+					}
+				}
+			} else {
+				t.Error(lib.ColorString("Answer is not equal to accepted output", lib.ColorRed))
+				t.Fatalf("Expected %v", existingOutput)
+			}
+
+			t.Log("")
+			t.Log("=================================")
+		})
+	})
+	if !ok {
+		t.FailNow()
+		return
+	}
+}
