d5898180f67931068fd4251ac86f7bd15439d1aa
Sumner Evans <me@sumnerevans.com>
2023-12-07 00:06:39 -0700

    2023/07: solve part 2
    Signed-off-by: Sumner Evans <me@sumnerevans.com>

--- y2023/d07/d07.ml
+++ y2023/d07/d07.ml
@@ -35,8 +35,8 @@ let int_of_hand hand =
   | OnePair -> 5
   | HighCard -> 6
 
-let int_of_card card_str =
-  match card_str with
+let int_of_card_p1 card =
+  match card with
   | '2' -> 13
   | '3' -> 12
   | '4' -> 11
@@ -52,14 +52,41 @@ let int_of_card card_str =
   | 'A' -> 1
   | _ -> failwith "Invalid card"
 
-let ints_of_hand_str hand_str =
+let int_of_card_p2 card =
+  match card with
+  | 'J' -> 14
+  | '2' -> 13
+  | '3' -> 12
+  | '4' -> 11
+  | '5' -> 10
+  | '6' -> 9
+  | '7' -> 8
+  | '8' -> 7
+  | '9' -> 6
+  | 'T' -> 5
+  | 'Q' -> 3
+  | 'K' -> 2
+  | 'A' -> 1
+  | _ -> failwith "Invalid card"
+
+let ints_of_hand_str_p1 hand_str =
   match String.to_seq hand_str |> List.of_seq with
   | [a; b; c; d; e] ->
-      ( int_of_card a
-      , int_of_card b
-      , int_of_card c
-      , int_of_card d
-      , int_of_card e )
+      ( int_of_card_p1 a
+      , int_of_card_p1 b
+      , int_of_card_p1 c
+      , int_of_card_p1 d
+      , int_of_card_p1 e )
+  | _ -> failwith "Invalid hand"
+
+let ints_of_hand_str_p2 hand_str =
+  match String.to_seq hand_str |> List.of_seq with
+  | [a; b; c; d; e] ->
+      ( int_of_card_p2 a
+      , int_of_card_p2 b
+      , int_of_card_p2 c
+      , int_of_card_p2 d
+      , int_of_card_p2 e )
   | _ -> failwith "Invalid hand"
 
 let sort_str s = String.to_seq s |> List.of_seq |> List.sort Char.compare
@@ -74,66 +101,116 @@ let compress s =
   in
   match sort_str s with [] -> [] | x :: xs -> compress' xs x 1
 
-let parse_hand hand_str =
-  printf "%s\n" hand_str ;
-  List.iter
-    (fun (count, c) -> printf "(%d, %c) " count c)
-    (List.sort compare (compress hand_str)) ;
-  print_newline () ;
+let parse_hand_p1 hand_str =
   match List.sort compare (compress hand_str) with
-  | [(5, _)] -> (FiveOfAKind, ints_of_hand_str hand_str)
-  | [(1, _); (4, _)] -> (FourOfAKind, ints_of_hand_str hand_str)
-  | [(2, _); (3, _)] -> (FullHouse, ints_of_hand_str hand_str)
-  | [_; _; (3, _)] -> (ThreeOfAKind, ints_of_hand_str hand_str)
-  | [_; (2, _); (2, _)] -> (TwoPair, ints_of_hand_str hand_str)
-  | [_; _; _; (2, _)] -> (OnePair, ints_of_hand_str hand_str)
-  | _ -> (HighCard, ints_of_hand_str hand_str)
+  | [(5, _)] -> (FiveOfAKind, ints_of_hand_str_p1 hand_str)
+  | [(1, _); (4, _)] -> (FourOfAKind, ints_of_hand_str_p1 hand_str)
+  | [(2, _); (3, _)] -> (FullHouse, ints_of_hand_str_p1 hand_str)
+  | [_; _; (3, _)] -> (ThreeOfAKind, ints_of_hand_str_p1 hand_str)
+  | [_; (2, _); (2, _)] -> (TwoPair, ints_of_hand_str_p1 hand_str)
+  | [_; _; _; (2, _)] -> (OnePair, ints_of_hand_str_p1 hand_str)
+  | _ -> (HighCard, ints_of_hand_str_p1 hand_str)
 
 let compare_hands (hand1kind, hand1str) (hand2kind, hand2str) =
   compare (int_of_hand hand1kind, hand1str) (int_of_hand hand2kind, hand2str)
 
-let part1 hands =
+let part1 =
+  let hands =
+    List.map
+      (fun s ->
+        match String.split_on_char ' ' s with
+        | [hand; bet] -> (parse_hand_p1 hand, int_of_string bet)
+        | _ -> failwith "Invalid input" )
+      (read_file file)
+  in
   List.fold_left ( + ) 0
   @@ List.mapi
-       (fun i (_, bet) ->
-         printf "%d %d\n" (i + 1) bet ;
+       (fun i (_, bet) -> (i + 1) * bet)
+       (List.sort
+          (fun (hand1, _) (hand2, _) -> compare_hands hand2 hand1)
+          hands )
+
+let parse_hand_p2 hand_str =
+  match List.sort compare (compress hand_str) with
+  | [(5, _)] -> (FiveOfAKind, ints_of_hand_str_p2 hand_str)
+  | [(1, 'J'); (4, _)] -> (FiveOfAKind, ints_of_hand_str_p2 hand_str)
+  | [(1, _); (4, 'J')] -> (FiveOfAKind, ints_of_hand_str_p2 hand_str)
+  | [(2, 'J'); (3, _)] -> (FiveOfAKind, ints_of_hand_str_p2 hand_str)
+  | [(2, _); (3, 'J')] -> (FiveOfAKind, ints_of_hand_str_p2 hand_str)
+  | [(1, _); (4, _)] -> (FourOfAKind, ints_of_hand_str_p2 hand_str)
+  | [(1, 'J'); _; (3, _)] -> (FourOfAKind, ints_of_hand_str_p2 hand_str)
+  | [_; (1, 'J'); (3, _)] -> (FourOfAKind, ints_of_hand_str_p2 hand_str)
+  | [_; (1, _); (3, 'J')] -> (FourOfAKind, ints_of_hand_str_p2 hand_str)
+  | [(1, _); (2, 'J'); (2, _)] -> (FourOfAKind, ints_of_hand_str_p2 hand_str)
+  | [(1, _); (2, _); (2, 'J')] -> (FourOfAKind, ints_of_hand_str_p2 hand_str)
+  | [(2, _); (3, _)] -> (FullHouse, ints_of_hand_str_p2 hand_str)
+  | [(1, 'J'); (2, _); (2, _)] -> (FullHouse, ints_of_hand_str_p2 hand_str)
+  | [(1, 'J'); (1, _); (1, _); (2, _)] ->
+      (ThreeOfAKind, ints_of_hand_str_p2 hand_str)
+  | [(1, _); (1, 'J'); (1, _); (2, _)] ->
+      (ThreeOfAKind, ints_of_hand_str_p2 hand_str)
+  | [(1, _); (1, _); (1, 'J'); (2, _)] ->
+      (ThreeOfAKind, ints_of_hand_str_p2 hand_str)
+  | [(1, _); (1, _); (1, _); (2, 'J')] ->
+      (ThreeOfAKind, ints_of_hand_str_p2 hand_str)
+  | [_; _; (3, _)] -> (ThreeOfAKind, ints_of_hand_str_p2 hand_str)
+  | [_; (2, _); (2, _)] -> (TwoPair, ints_of_hand_str_p2 hand_str)
+  | [_; _; _; (2, _)] -> (OnePair, ints_of_hand_str_p2 hand_str)
+  | [(1, 'J'); (1, _); (1, _); (1, _); (1, _)] ->
+      (OnePair, ints_of_hand_str_p2 hand_str)
+  | [(1, _); (1, 'J'); (1, _); (1, _); (1, _)] ->
+      (OnePair, ints_of_hand_str_p2 hand_str)
+  | [(1, _); (1, _); (1, 'J'); (1, _); (1, _)] ->
+      (OnePair, ints_of_hand_str_p2 hand_str)
+  | [(1, _); (1, _); (1, _); (1, 'J'); (1, _)] ->
+      (OnePair, ints_of_hand_str_p2 hand_str)
+  | [(1, _); (1, _); (1, _); (1, _); (1, 'J')] ->
+      (OnePair, ints_of_hand_str_p2 hand_str)
+  | _ -> (HighCard, ints_of_hand_str_p2 hand_str)
+
+let part2 =
+  let hands =
+    List.map
+      (fun s ->
+        match String.split_on_char ' ' s with
+        | [hand; bet] ->
+            (* let hand_type, (a, b, c, d, e) = parse_hand_p2 hand in *)
+            (* printf "%s %d (%d, %d, %d, %d, %d)\n" hand *)
+            (*   (int_of_hand hand_type) a b c d e ; *)
+            (* flush stdout ; *)
+            (* let _ = *)
+            (*   match ints_of_hand_str_p2 hand with *)
+            (*   | 14, _, _, _, _ -> input_line stdin *)
+            (*   | _, 14, _, _, _ -> input_line stdin *)
+            (*   | _, _, 14, _, _ -> input_line stdin *)
+            (*   | _, _, _, 14, _ -> input_line stdin *)
+            (*   | _, _, _, _, 14 -> input_line stdin *)
+            (*   | _ -> "" *)
+            (* in *)
+            (* () ; *)
+            (parse_hand_p2 hand, int_of_string bet)
+        | _ -> failwith "Invalid input" )
+      (read_file file)
+  in
+  List.fold_left ( + ) 0
+  @@ List.mapi
+       (fun i ((hand_type, (a, b, c, d, e)), bet) ->
+         printf "%d (%d, %d, %d, %d, %d) %d %d" (int_of_hand hand_type) a b c
+           d e (i + 1) bet ;
+         print_newline () ;
          (i + 1) * bet )
        (List.sort
           (fun (hand1, _) (hand2, _) -> compare_hands hand2 hand1)
           hands )
 
-(* let part2 nums = *)
-(*   let rec windows3 l = *)
-(*     match l with *)
-(*     | [] -> [] *)
-(*     | [a] -> [] *)
-(*     | [a; b] -> [] *)
-(*     | a :: b :: c :: more -> (a, b, c) :: windows3 (b :: c :: more) *)
-(*   in *)
-(*   part1 (List.map (fun (a, b, c) -> a + b + c) (windows3 nums)) *)
-
 (* Run the program *)
 let () =
-  let ht, (a, b, c, d, e) = parse_hand "432AA" in
-  printf "%d (%d, %d, %d, %d, %d)\n" (int_of_hand ht) a b c d e ;
-  print_newline () ;
-  print_newline () ;
-  print_newline () ;
-  print_newline () ;
   (* Input parsing *)
-  let hands =
-    List.map
-      (fun s ->
-        match String.split_on_char ' ' s with
-        | [hand; bet] -> (parse_hand hand, int_of_string bet)
-        | _ -> failwith "Invalid input" )
-      (read_file file)
-  in
   (* Part 1 *)
   Printf.printf "Part 1:\n" ;
-  print_int (part1 hands) ;
+  print_int part1 ;
   print_newline () ;
   (* Part 2 *)
   Printf.printf "Part 2:\n" ;
-  (* print_int (part2 lines) ; *)
+  print_int part2 ;
   print_newline ()
