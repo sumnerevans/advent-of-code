0e20223c2b32ebaba2837d50463a045a5454d268
Sumner Evans <me@sumnerevans.com>
2025-12-01 22:29:38 -0700

    2025/02: solve
    Signed-off-by: Sumner Evans <me@sumnerevans.com>

---
+++ y2025/d02/Program.cs
@@ -0,0 +1,70 @@
+using System.Diagnostics;
+using System.Text.RegularExpressions;
+
+public static class Program
+{
+    readonly record struct Range(long Start, long End);
+
+    public static void Main()
+    {
+        using (var reader = new StreamReader("02.txt"))
+        // using (var reader = new StreamReader("02.test.01.txt"))
+        // using (var reader = new StreamReader("02.test.02.txt"))
+        // using (var reader = new StreamReader("02.test.03.txt"))
+        {
+            var ranges = reader
+                .ReadLine()!
+                .Split(",")
+                .Where(s => !string.IsNullOrWhiteSpace(s))
+                .Select(s =>
+                {
+                    var parts = s.Split("-");
+                    return new Range(long.Parse(parts[0]), long.Parse(parts[1]));
+                })
+                .ToList();
+
+            Console.WriteLine($"Part 1: {Part1(ranges)}");
+            Console.WriteLine($"Part 2: {Part2(ranges)}");
+            Console.WriteLine(new char[]{'a'}.ToString());
+        }
+    }
+
+    static long Part1(List<Range> ranges) => ranges.SelectMany(InvalidPart1).Sum();
+
+    static IEnumerable<long> InvalidPart1(Range range)
+    {
+        for (long i = range.Start; i <= range.End; i++)
+        {
+            string num = i.ToString();
+            if (num.Length % 2 != 0)
+                continue;
+
+            if (num.Substring(0, num.Length / 2) == num.Substring(num.Length / 2))
+                yield return i;
+        }
+    }
+
+    static long Part2(List<Range> ranges)
+    {
+        return ranges.SelectMany(InvalidPart2).Sum();
+    }
+
+    static IEnumerable<long> InvalidPart2(Range range)
+    {
+        for (long i = range.Start; i <= range.End; i++)
+        {
+            string num = i.ToString();
+
+            for (int length = 1; length <= num.Length / 2; length++)
+            {
+                var chunks = num.Chunk(length).Select(c => new string(c)).ToList();
+                var first = chunks.First();
+                if (chunks.Skip(1).All(c => c == first))
+                {
+                    yield return i;
+                    break;
+                }
+            }
+        }
+    }
+}
