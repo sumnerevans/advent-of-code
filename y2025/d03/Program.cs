57c0147e5db224c2a74b1a7e83f9d4d9ad34e808
Sumner Evans <me@sumnerevans.com>
2025-12-02 23:11:09 -0700

    2025/03: cleanup to make slightly more idiomatic
    Signed-off-by: Sumner Evans <me@sumnerevans.com>

--- y2025/d03/Program.cs
+++ y2025/d03/Program.cs
@@ -16,52 +16,29 @@ public static class Program
         }
     }
 
-    static long Part1(List<string> banks) => banks.Select(b => MaxJoltage1(b).Max()).Sum();
-
-    static IEnumerable<long> MaxJoltage1(string bank)
-    {
-        for (int i = 0; i < bank.Length; i++)
-        {
-            for (int j = i + 1; j < bank.Length; j++)
-            {
-                yield return Convert.ToInt32(new string(new char[] { bank[i], bank[j] }));
-            }
-        }
-    }
+    static long Part1(List<string> banks) => banks.Select(b => long.Parse(MaxJoltage(b, 2))).Sum();
 
     static long Part2(List<string> banks) => banks.Select(b => long.Parse(MaxJoltage(b, 12))).Sum();
 
-    static string MaxJoltage(string bank, int pick)
-    {
-        var cache = new Dictionary<(int, int), string>();
-        return MaxJoltageRec(cache, bank, 0, pick);
-    }
+    static string MaxJoltage(string bank, int pick) =>
+        MaxJoltageRec(new Dictionary<(int, int), string>(), bank, 0, pick);
 
-    static string MaxJoltageRec(Dictionary<(int, int), string> cache, string bank, int start, int pick)
+    static string MaxJoltageRec(IDictionary<(int, int), string> cache, string bank, int start, int pick)
     {
-        if (cache.TryGetValue((start, pick), out string val))
-            return val;
+        if (cache.TryGetValue((start, pick), out string cached))
+            return cached;
         if ((bank.Length - start) - pick < 0)
             return "";
         if (pick == 0)
             return "";
 
-        var opt1 = bank[start] + MaxJoltageRec(cache, bank, start + 1, pick - 1);
-        var opt2 = MaxJoltageRec(cache, bank, start + 1, pick);
-        if (opt2.Length == 0)
-        {
-            cache.Add((start, pick), opt1);
-            return opt1;
-        }
-        if (Convert.ToInt64(opt1) > Convert.ToInt64(opt2))
-        {
-            cache.Add((start, pick), opt1);
-            return opt1;
-        }
-        else
-        {
-            cache.Add((start, pick), opt2);
-            return opt2;
-        }
+        string withCurrentChar = bank[start] + MaxJoltageRec(cache, bank, start + 1, pick - 1);
+        string withoutCurrentChar = MaxJoltageRec(cache, bank, start + 1, pick);
+        string selected =
+            (withoutCurrentChar.Length == 0 || Convert.ToInt64(withCurrentChar) > Convert.ToInt64(withoutCurrentChar))
+                ? withCurrentChar
+                : withoutCurrentChar;
+        cache.Add((start, pick), selected);
+        return selected;
     }
 }
