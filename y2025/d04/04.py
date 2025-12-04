14f748266216bd15ce31ee0a7e517ab5b7497832
Sumner Evans <me@sumnerevans.com>
2025-12-03 22:50:57 -0700

    2025/04: add bad Python impl
    Signed-off-by: Sumner Evans <me@sumnerevans.com>

---
+++ y2025/d04/04.py
@@ -0,0 +1,291 @@
+#! /usr/bin/env python3
+
+import functools as ft
+import heapq
+import itertools as it
+import math
+import operator
+import os
+import re
+import string
+import sys
+import time
+from collections import defaultdict
+from copy import deepcopy
+from enum import Enum, IntEnum
+from fractions import Fraction
+from typing import (Callable, Dict, Generator, Iterable, Iterator, List, Match, Optional, Set,
+                    Tuple, TypeVar, Union)
+
+TEST = True
+DEBUG = False
+STDIN = False
+INFILENAME = "04.txt"
+TESTFILENAME = "04.test.01.txt"
+for arg in sys.argv:
+    if arg == "--notest":
+        TEST = False
+    if arg == "--debug":
+        DEBUG = True
+    if arg == "--stdin":
+        STDIN = True
+
+
+class bcolors:
+    HEADER = "\033[95m"
+    OKBLUE = "\033[94m"
+    OKCYAN = "\033[96m"
+    OKGREEN = "\033[92m"
+    WARNING = "\033[93m"
+    FAIL = "\033[91m"
+    ENDC = "\033[0m"
+    BOLD = "\033[1m"
+    UNDERLINE = "\033[4m"
+
+
+# Type variables
+K = TypeVar("K")
+V = TypeVar("V")
+
+
+# Enums
+class BoundsType(Enum):
+    """
+    Different types of bounds to use while computing adjacencies.
+
+    RANGE:      [low, high)
+    INCLUSIVE:  [low, high]
+    EXCLUSIVE:  (low, high)
+    """
+
+    RANGE = "range"
+    INCLUSIVE = "inclusive"
+    EXCLUSIVE = "exclusive"
+
+
+class AdjacenciesType(Enum):
+    """
+    Different types of bounds to use while computing adjacencies.
+
+    COMPASS: only directions where a single dimension changes (without diagonals)
+    ALL:     all adjacencies including diagonals
+    """
+
+    COMPASS = "compass"
+    ALL = "all"
+
+
+def grid_adjs(
+    coord: Tuple[int, ...],
+    bounds: Tuple[Tuple[int, int], ...] = None,
+    adjs_type: AdjacenciesType = AdjacenciesType.COMPASS,
+    bounds_type: BoundsType = BoundsType.RANGE,
+) -> Iterable[Tuple[int, ...]]:
+    """
+    Compute the compass adjacencies for a given :math:`n`-dimensional point. Bounds can
+    be specified, and only adjacent coordinates within those bounds will be returned.
+    Bounds can be specified as any one of the :class:`BoundsType`s.
+
+    :param coord: coordinate to calculate the adjacencies of
+    :param bounds: ``(high, low)`` tuples for each of the dimensions
+    :param adjs_type: the :class:`AdjacenciesType` to use
+    :param bounds_type: the :class:`BoundsType` to use
+    """
+    # Iterate through all of the deltas for the N dimensions of the coord. A delta is
+    # -1, 0, or 1 indicating that the adjacent cell is one lower, same level, or higher
+    # than the given coordinate.
+    for delta in it.product((-1, 0, 1), repeat=len(coord)):
+        if all(d == 0 for d in delta):
+            # This is the coord itself, skip.
+            continue
+
+        if adjs_type == AdjacenciesType.COMPASS:
+            if sum(map(abs, delta)) > 1:
+                # For compass adjacencies, we only care when there's only one dimension
+                # different than the coordinate.
+                continue
+
+        if bounds is not None:
+            in_bounds = True
+            for i, (d, (low, high)) in enumerate(zip(delta, bounds)):
+                if bounds_type == BoundsType.RANGE:
+                    in_bounds &= low <= coord[i] + d < high
+                elif bounds_type == BoundsType.INCLUSIVE:
+                    in_bounds &= low <= coord[i] + d <= high
+                elif bounds_type == BoundsType.EXCLUSIVE:
+                    in_bounds &= low < coord[i] + d < high
+                if not in_bounds:
+                    continue
+
+            if not in_bounds:
+                continue
+        yield tuple(c + d for c, d in zip(coord, delta))
+
+
+print(f"\n{'=' * 30}\n")
+
+# Read the input
+if STDIN:
+    input_lines: List[str] = [l.strip() for l in sys.stdin.readlines()]
+else:
+    with open(INFILENAME) as f:
+        input_lines: List[str] = [l.strip() for l in f.readlines()]
+
+# Try and read in the test file.
+try:
+    with open(TESTFILENAME) as f:
+        test_lines: List[str] = [l.strip() for l in f.readlines()]
+except Exception:
+    test_lines = []
+
+
+# Shared
+########################################################################################
+
+
+# Part 1
+########################################################################################
+print("Part 1:")
+
+
+def part1(lines: List[str], test: bool = False) -> int:
+    ans = 0
+
+    M = [x for x in lines]
+    # seq = [int(x) for x in lines[0].split(",")]
+    # L = [[int(x) for x in l] for l in linese
+
+    maxR = len(lines)
+    maxC = len(lines[0])
+    for r, line in enumerate(M):
+        for c, ch in enumerate(line):
+            if ch == "@":
+                adjs = grid_adjs((r, c), ((0, maxR), (0, maxC)), AdjacenciesType.ALL)
+                if sum(map(lambda a: 1 if M[a[0]][a[1]] == "@" else 0, adjs)) < 4:
+                    ans += 1
+
+    return ans
+
+
+# Run test on part 1
+if TEST:
+    print("Running test... ", end="")
+    if not test_lines:
+        print(f"{bcolors.FAIL}No test configured!{bcolors.ENDC}")
+    else:
+        test_ans_part1 = part1(test_lines, test=True)
+        expected = 13
+        if expected is None:
+            print(f"{bcolors.FAIL}No test configured!{bcolors.ENDC}")
+        elif test_ans_part1 == expected:
+            print(f"{bcolors.OKGREEN}PASS{bcolors.ENDC}")
+        else:
+            print(f"{bcolors.FAIL}FAIL{bcolors.ENDC}")
+            print(f"{bcolors.FAIL}Result: {test_ans_part1} != {expected}{bcolors.ENDC}")
+            assert False
+
+        print("Result:", test_ans_part1)
+        print()
+
+part1_start = time.time()
+print("Running input...")
+ans_part1 = part1(input_lines)
+part1_end = time.time()
+print("Result:", ans_part1)
+
+tries = [
+    # Store the attempts that failed here.
+]
+if tries:
+    print("Tries Part 1:", tries)
+    assert ans_part1 not in tries, "Same as an incorrect answer!"
+
+
+# Regression Test
+expected = 1549
+if expected is not None:
+    assert ans_part1 == expected
+
+# Part 2
+########################################################################################
+print("\nPart 2:")
+
+
+def part2(lines: List[str], test: bool = False) -> int:
+    ans = 0
+
+    M = [x for x in lines]
+
+    while True:
+        changed = 0
+        maxR = len(lines)
+        maxC = len(lines[0])
+        newM = []
+        for r, line in enumerate(M):
+            newM.append([])
+            for c, ch in enumerate(line):
+                if ch == "@":
+                    adjs = grid_adjs(
+                        (r, c), ((0, maxR), (0, maxC)), AdjacenciesType.ALL
+                    )
+                    if sum(map(lambda a: 1 if M[a[0]][a[1]] == "@" else 0, adjs)) < 4:
+                        changed += 1
+                        ans += 1
+                        newM[r].append("x")
+                    else:
+                        newM[r].append("@")
+                else:
+                    newM[r].append(ch)
+
+        M = newM
+
+        if changed == 0:
+            return ans
+
+
+# Run test on part 2
+if TEST:
+    print("Running test... ", end="")
+    if not test_lines:
+        print(f"{bcolors.FAIL}No test configured!{bcolors.ENDC}")
+    else:
+        test_ans_part2 = part2(test_lines, test=True)
+        expected = 43
+        if expected is None:
+            print(f"{bcolors.FAIL}No test configured!{bcolors.ENDC}")
+        elif test_ans_part2 == expected:
+            print(f"{bcolors.OKGREEN}PASS{bcolors.ENDC}")
+        else:
+            print(f"{bcolors.FAIL}FAIL{bcolors.ENDC}")
+            print(f"{bcolors.FAIL}Result: {test_ans_part2} != {expected}{bcolors.ENDC}")
+            assert False
+
+        print("Result:", test_ans_part2)
+        print()
+
+part2_start = time.time()
+print("Running input...")
+ans_part2 = part2(input_lines)
+part2_end = time.time()
+print("Result:", ans_part2)
+
+tries2 = [
+    # Store the attempts that failed here.
+]
+if tries2:
+    print("Tries Part 2:", tries2)
+    assert ans_part2 not in tries2, "Same as an incorrect answer!"
+
+# Regression Test
+expected = 8887
+if expected is not None:
+    assert ans_part2 == expected
+
+if DEBUG:
+    part1_time = part1_end - part1_start
+    part2_time = part2_end - part2_start
+    print()
+    print("DEBUG:")
+    print(f"Part 1: {part1_time * 1000}ms")
+    print(f"Part 2: {part2_time * 1000}ms")
+    print(f"TOTAL: {(part1_time + part2_time) * 1000}ms")
